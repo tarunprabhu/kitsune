0013789fc798e9368a57a5fff06ca99c5833272b
TB Schardl <neboat@mit.edu>
2018-02-11 19:38:54 +0000

    [Cilk] Re-enabled the '-ftapir=none' option for emitting Tapir without lowering.  This flag is intended for testing and debugging purposes.

--- clang/include/clang/Basic/LangOptions.h
+++ clang/include/clang/Basic/LangOptions.h
@@ -506,7 +506,7 @@ public:
   bool CheckNew = false;
 
   /// \brief Runtime target for Tapir.
-  TapirTargetType TapirTarget = TapirTargetType::None;
+  TapirTargetType TapirTarget = TapirTargetType::Last_TapirTargetType;
 
   LangOptions();
 
