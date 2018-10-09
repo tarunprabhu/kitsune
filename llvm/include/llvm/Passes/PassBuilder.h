e8bc3af9f320367d08ab77f56320349c1605209b
TB Schardl <neboat@mit.edu>
2018-10-09 16:12:15 +0000

    [LTO] Adding experimental support for Tapir lowering in LTO and ThinLTO pipelines.

--- llvm/include/llvm/Passes/PassBuilder.h
+++ llvm/include/llvm/Passes/PassBuilder.h
@@ -270,7 +270,8 @@ public:
   /// build them.
   ModulePassManager
   buildThinLTODefaultPipeline(OptimizationLevel Level,
-                              const ModuleSummaryIndex *ImportSummary);
+                              const ModuleSummaryIndex *ImportSummary,
+                              bool LowerTapir = false);
 
   /// Build a pre-link, LTO-targeting default optimization pipeline to a pass
   /// manager.
@@ -298,7 +299,8 @@ public:
   /// require some transformations for semantic reasons, they should explicitly
   /// build them.
   ModulePassManager buildLTODefaultPipeline(OptimizationLevel Level,
-                                            ModuleSummaryIndex *ExportSummary);
+                                            ModuleSummaryIndex *ExportSummary,
+                                            bool LowerTapir = false);
 
   /// Build an O0 pipeline with the minimal semantically required passes.
   ///
