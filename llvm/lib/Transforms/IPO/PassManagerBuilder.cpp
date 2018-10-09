92cc0a5d186f5152bc2a9004e82aec99ec6efb4d
TB Schardl <neboat@mit.edu>
2018-10-09 16:07:48 +0000

    [TargetLibraryInfo] Moving Tapir target for lowering into TargetLibraryInfo.

--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -302,6 +302,19 @@ void PassManagerBuilder::populateModulePassManager(
       Inliner = nullptr;
     }
 
+<<<<<<< HEAD
+=======
+    // Add passes to run just before Tapir lowering.
+    addExtensionsToPM(EP_TapirLate, MPM);
+
+    if (TapirTargetID::None != TapirTarget) {
+      // MPM.add(createAnalyzeTapirPass());
+      MPM.add(createLowerTapirToTargetPass());
+      // The lowering pass may leave cruft around.  Clean it up.
+      MPM.add(createCFGSimplificationPass());
+    }
+
+>>>>>>> 100e91461aa1 ([TargetLibraryInfo] Moving Tapir target for lowering into TargetLibraryInfo.)
     // FIXME: The BarrierNoopPass is a HACK! The inliner pass above implicitly
     // creates a CGSCC pass manager, but we don't want to add extensions into
     // that pass manager. To prevent this we insert a no-op module pass to reset
@@ -318,6 +331,22 @@ void PassManagerBuilder::populateModulePassManager(
 
   addInitialAliasAnalysisPasses(MPM);
 
+<<<<<<< HEAD
+=======
+  bool RerunAfterTapirLowering = false;
+  bool TapirHasBeenLowered = (TapirTargetID::None == TapirTarget);
+
+  if ((TapirTargetID::None != TapirTarget) && DisableTapirOpts) { // -fdetach
+    // MPM.add(createAnalyzeTapirPass());
+    MPM.add(createLowerTapirToTargetPass());
+    TapirHasBeenLowered = true;
+  }
+
+  do {
+    RerunAfterTapirLowering =
+       !TapirHasBeenLowered && (ParallelLevel > 0) && !PrepareForThinLTO;
+
+>>>>>>> 100e91461aa1 ([TargetLibraryInfo] Moving Tapir target for lowering into TargetLibraryInfo.)
   // Infer attributes about declarations if possible.
   MPM.add(createInferFunctionAttrsLegacyPass());
 
@@ -448,6 +477,65 @@ void PassManagerBuilder::populateModulePassManager(
   // resulted in single-entry-single-exit or empty blocks. Clean up the CFG.
   MPM.add(createCFGSimplificationPass(
       SimplifyCFGOptions().convertSwitchRangeToICmp(true)));
+<<<<<<< HEAD
+=======
+  MPM.add(createTaskSimplifyPass());
+
+  if (RerunAfterTapirLowering || (TapirTargetID::None == TapirTarget))
+    // Add passes to run just before Tapir lowering.
+    addExtensionsToPM(EP_TapirLate, MPM);
+
+  if (!TapirHasBeenLowered) {
+    // MPM.add(createAnalyzeTapirPass());
+    // First handle Tapir loops.  First, simplify their induction variables.
+    MPM.add(createIndVarSimplifyPass());
+    // Re-rotate loops in all our loop nests. These may have fallout out of
+    // rotated form due to GVN or other transformations, and loop spawning
+    // relies on the rotated form.  Disable header duplication at -Oz.
+    MPM.add(createLoopRotatePass(SizeLevel == 2 ? 0 : -1));
+    // Outline Tapir loops as needed.
+    MPM.add(createLoopSpawningTIPass());
+
+    // The LoopSpawning pass may leave cruft around.  Clean it up.
+    MPM.add(createCFGSimplificationPass(
+        SimplifyCFGOptions().convertSwitchRangeToICmp(true)));
+    MPM.add(createPostOrderFunctionAttrsLegacyPass());
+    if (OptLevel > 2)
+      MPM.add(createArgumentPromotionPass()); // Scalarize uninlined fn args
+    addFunctionSimplificationPasses(MPM);
+    MPM.add(createReversePostOrderFunctionAttrsPass());
+    if (MergeFunctions)
+      MPM.add(createMergeFunctionsPass());
+    MPM.add(createBarrierNoopPass());
+    // addFunctionSimplificationPasses(MPM);
+
+    // Now lower Tapir to Target runtime calls.
+    //
+    // TODO: Make this sequence of passes check the library info for the target
+    // parallel RTS.
+
+    MPM.add(createLowerTapirToTargetPass());
+    // The lowering pass introduces new functions and may leave cruft around.
+    // Clean it up.
+    MPM.add(createCFGSimplificationPass(
+        SimplifyCFGOptions().convertSwitchRangeToICmp(true)));
+    MPM.add(createPostOrderFunctionAttrsLegacyPass());
+    if (OptLevel > 2)
+      MPM.add(createArgumentPromotionPass()); // Scalarize uninlined fn args
+    addFunctionSimplificationPasses(MPM);
+    MPM.add(createReversePostOrderFunctionAttrsPass());
+    if (MergeFunctions)
+      MPM.add(createMergeFunctionsPass());
+    MPM.add(createBarrierNoopPass());
+
+    TapirHasBeenLowered = true;
+  }
+  } while (RerunAfterTapirLowering);
+
+  addExtensionsToPM(EP_OptimizerLast, MPM);
+
+  MPM.add(createAnnotationRemarksLegacyPass());
+>>>>>>> 100e91461aa1 ([TargetLibraryInfo] Moving Tapir target for lowering into TargetLibraryInfo.)
 }
 
 LLVMPassManagerBuilderRef LLVMPassManagerBuilderCreate() {
