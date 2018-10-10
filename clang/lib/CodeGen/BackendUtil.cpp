d324a2a384bb8222cdf5751801f4fb6278ed724e
TB Schardl <neboat@mit.edu>
2018-10-10 22:37:37 +0000

    [BackendUtil] Using TapirTarget value defined in TargetLibraryInfo.

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -730,7 +730,8 @@ void EmitAssemblyHelper::CreatePasses(legacy::PassManager &MPM,
 
   if (CodeGenOpts.TapirEarlyOutline) PMBuilder.DisableTapirOpts = true;
   if (CodeGenOpts.TapirRhino) PMBuilder.Rhino = true;
-  PMBuilder.TapirTarget = CodeGenOpts.getTapirTarget();
+  if (TLII->hasTapirTarget())
+    PMBuilder.TapirTarget = TLII->getTapirTarget();
 
   PMBuilder.SizeLevel = CodeGenOpts.OptimizeSize;
   PMBuilder.SLPVectorize = CodeGenOpts.VectorizeSLP;
@@ -1657,6 +1658,7 @@ static void runThinLTOBackend(
   Conf.RemarksFormat = CGOpts.OptRecordFormat;
   Conf.SplitDwarfFile = CGOpts.SplitDwarfFile;
   Conf.SplitDwarfOutput = CGOpts.SplitDwarfOutput;
+  Conf.TapirTarget = CGOpts.getTapirTarget();
   switch (Action) {
   case Backend_EmitNothing:
     Conf.PreCodeGenModuleHook = [](size_t Task, const Module &Mod) {
