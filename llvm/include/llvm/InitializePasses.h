eeb29cf50054f29f878dc9e852aac54e7d1bb9cd
TB Schardl <neboat@mit.edu>
2018-10-17 18:53:22 +0000

    [DetachSSA] Deprecating DetachSSA pass, in favor of task analysis pass.

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -132,8 +132,6 @@ void initializeDelinearizationPass(PassRegistry&);
 void initializeDemandedBitsWrapperPassPass(PassRegistry&);
 void initializeDependenceAnalysisPass(PassRegistry&);
 void initializeDependenceAnalysisWrapperPassPass(PassRegistry&);
-void initializeDetachSSAPrinterLegacyPassPass(PassRegistry&);
-void initializeDetachSSAWrapperPassPass(PassRegistry&);
 void initializeDetectDeadLanesPass(PassRegistry&);
 void initializeDivRemPairsLegacyPassPass(PassRegistry&);
 void initializeDomOnlyPrinterWrapperPassPass(PassRegistry &);
