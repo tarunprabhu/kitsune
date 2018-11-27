04a0d0b8b362f7907b175e767041860ed306e2ad
TB Schardl <neboat@mit.edu>
2018-11-27 16:29:49 +0000

    [DataRaceFreeAliasAnalysis] Add prototype alias analysis that deduces modref info from Tapir parallel constructs assuming the program is DRF.

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -125,6 +125,7 @@ void initializeDAHPass(PassRegistry&);
 void initializeDCELegacyPassPass(PassRegistry&);
 void initializeDFAJumpThreadingLegacyPassPass(PassRegistry &);
 void initializeDSELegacyPassPass(PassRegistry&);
+void initializeDRFAAWrapperPassPass(PassRegistry&);
 void initializeDataFlowSanitizerLegacyPassPass(PassRegistry &);
 void initializeDeadMachineInstructionElimPass(PassRegistry&);
 void initializeDebugifyMachineModulePass(PassRegistry &);
