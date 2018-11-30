501a847a5a573bdd7f5fbeec02835c14de27e45a
TB Schardl <neboat@mit.edu>
2018-11-30 20:11:48 +0000

    [MemorySSA] Prototype support to use the DRF assumption when analyzing clobbering definitions.

--- llvm/include/llvm/Analysis/MemorySSA.h
+++ llvm/include/llvm/Analysis/MemorySSA.h
@@ -117,6 +117,8 @@ class MemorySSAWalker;
 class Module;
 class Use;
 class Value;
+class TaskInfo;
+class LLVMContext;
 class raw_ostream;
 
 namespace MSSAHelpers {
@@ -699,7 +701,7 @@ DEFINE_TRANSPARENT_OPERAND_ACCESSORS(MemoryPhi, MemoryAccess)
 /// accesses.
 class MemorySSA {
 public:
-  MemorySSA(Function &, AliasAnalysis *, DominatorTree *);
+  MemorySSA(Function &, AliasAnalysis *, DominatorTree *, TaskInfo * = nullptr);
 
   // MemorySSA must remain where it's constructed; Walkers it creates store
   // pointers to it.
@@ -871,6 +873,7 @@ private:
   void renumberBlock(const BasicBlock *) const;
   AliasAnalysis *AA = nullptr;
   DominatorTree *DT;
+  TaskInfo *TI;
   Function &F;
 
   // Memory SSA mappings
@@ -915,7 +918,7 @@ protected:
 
   // This function should not be used by new passes.
   static bool defClobbersUseOrDef(MemoryDef *MD, const MemoryUseOrDef *MU,
-                                  AliasAnalysis &AA);
+                                  AliasAnalysis &AA, TaskInfo *TI = nullptr);
 };
 
 /// An analysis that produces \c MemorySSA for a function.
