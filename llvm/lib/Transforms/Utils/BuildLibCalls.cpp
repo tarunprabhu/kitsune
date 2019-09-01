4ac66f73a541caf29feb0b77a560acf90266cba9
TB Schardl <neboat@mit.edu>
2019-09-01 19:02:20 +0000

    [TargetLibraryInfo] Added TLI entry for aligned_alloc.

--- llvm/lib/Transforms/Utils/BuildLibCalls.cpp
+++ llvm/lib/Transforms/Utils/BuildLibCalls.cpp
@@ -462,6 +462,10 @@ bool llvm::inferNonMandatoryLibFuncAttrs(Function &F,
     Changed |= setRetDoesNotAlias(F);
     Changed |= setWillReturn(F);
     break;
+  case LibFunc_aligned_alloc:
+    Changed |= setDoesNotThrow(F);
+    Changed |= setRetDoesNotAlias(F);
+    break;
   case LibFunc_memcmp:
     Changed |= setOnlyAccessesArgMemory(F);
     Changed |= setOnlyReadsMemory(F);
