e5411a1890f409190c9ddc0ae3d8023843ef529f
Alexis Perry-Holby <aperry@lanl.gov>
2019-12-04 12:29:00 -0700

    CMake corrections to require the use of both the Realm library and the kitsune-rt Realm wrapper library in order to enable the Realm backend

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -845,7 +845,8 @@ void ToolChain::AddKitsuneLibArgs(const ArgList &Args,
       } else if (Name == "realm") {
 	if (KITSUNE_ENABLE_REALM) { 
 	  CmdArgs.push_back("-L" KITSUNE_REALM_LIBRARY_DIR);
-	  CmdArgs.push_back("-rpath=" KITSUNE_REALM_LIBRARY_DIR);
+	  CmdArgs.push_back("-L" KITSUNE_REALM_WRAPPER_LIBRARY_DIR);
+	  CmdArgs.push_back("-rpath=" KITSUNE_REALM_LIBRARY_DIR ":" KITSUNE_REALM_WRAPPER_LIBRARY_DIR);
 	  ExtractArgsFromString(KITSUNE_REALM_LINK_LIBS, CmdArgs, Args);
 	} else {
 	  getDriver().Diag(diag::warn_realm_missing_build_params);	  
