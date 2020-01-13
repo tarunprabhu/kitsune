bccd32768685d359bc0b8c7d01d03e5b8e4f6aa6
TB Schardl <neboat@mit.edu>
2020-01-13 17:03:26 +0000

    [Tapir] Clean up old, unmaintained code.

--- llvm/include/llvm/LinkAllPasses.h
+++ llvm/include/llvm/LinkAllPasses.h
@@ -78,7 +78,6 @@ namespace {
       (void) llvm::createBitTrackingDCEPass();
       (void)llvm::createOpenMPOptCGSCCLegacyPass();
       (void) llvm::createAlignmentFromAssumptionsPass();
-      (void) llvm::createAnalyzeTapirPass();
       (void) llvm::createBasicAAWrapperPass();
       (void) llvm::createSCEVAAWrapperPass();
       (void) llvm::createTypeBasedAAWrapperPass();
--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -172,10 +172,6 @@ static cl::opt<bool> EnableSerializeSmallTasks(
   "enable-serialize-small-tasks", cl::Hidden, cl::init(false),
   cl::desc("Serialize any Tapir tasks found to be unprofitable (default = off)"));
 
-static cl::opt<bool> EnableDRFAA(
-    "enable-drf-aa", cl::init(false), cl::Hidden,
-    cl::desc("Enable AA based on the data-race-free assumption (default = off)"));
-
 static cl::opt<bool> DisableTapirOpts(
     "disable-tapir-opts", cl::init(false), cl::Hidden,
     cl::desc("Disable Tapir optimizations by outlining Tapir tasks early"));
@@ -500,8 +496,6 @@ void PassManagerBuilder::addVectorPasses(legacy::PassManagerBase &PM,
 
   if (EnableSerializeSmallTasks)
     PM.add(createSerializeSmallTasksPass());
-  if (EnableDRFAA)
-    PM.add(createDRFScopedNoAliasWrapperPass());
 
   if (!IsFullLTO) {
     // Eliminate loads by forwarding stores from the previous iteration to loads
@@ -622,7 +616,6 @@ void PassManagerBuilder::populateModulePassManager(
     addExtensionsToPM(EP_TapirLoopEnd, MPM);
 
     if (TapirTargetID::None != TapirTarget) {
-      // MPM.add(createAnalyzeTapirPass());
       MPM.add(createLowerTapirToTargetPass());
       // The lowering pass may leave cruft around.  Clean it up.
       MPM.add(createCFGSimplificationPass());
@@ -648,8 +641,6 @@ void PassManagerBuilder::populateModulePassManager(
   if (LibraryInfo)
     MPM.add(new TargetLibraryInfoWrapperPass(*LibraryInfo));
 
-  if (EnableDRFAA)
-    MPM.add(createDRFScopedNoAliasWrapperPass());
   addInitialAliasAnalysisPasses(MPM);
 
   bool RerunAfterTapirLowering = false;
@@ -828,8 +819,6 @@ void PassManagerBuilder::populateModulePassManager(
     MPM.add(createJumpThreadingPass());         // Thread jumps
     MPM.add(createCorrelatedValuePropagationPass());
     addInstructionCombiningPass(MPM);
-    if (EnableDRFAA)
-      MPM.add(createDRFScopedNoAliasWrapperPass());
   }
 
   addExtensionsToPM(EP_VectorizerStart, MPM);
@@ -892,7 +881,6 @@ void PassManagerBuilder::populateModulePassManager(
     addExtensionsToPM(EP_TapirLate, MPM);
 
   if (!TapirHasBeenLowered) {
-    // MPM.add(createAnalyzeTapirPass());
     // First handle Tapir loops.  First, simplify their induction variables.
     MPM.add(createIndVarSimplifyPass());
     // Re-rotate loops in all our loop nests. These may have fallout out of
