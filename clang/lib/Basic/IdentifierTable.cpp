fe4b986f571911fc4b5f52bc7ba60f2df60130a9
George Stelle <stelleg@lanl.gov>
2020-02-24 15:37:32 -0700

    Removed deleted header include

--- clang/lib/Basic/IdentifierTable.cpp
+++ clang/lib/Basic/IdentifierTable.cpp
@@ -28,7 +28,6 @@
 #include "llvm/Support/raw_ostream.h"
 #include "clang/Basic/Tapir.h"
 #include <cassert>
-#include "llvm/Transforms/Tapir/TapirTypes.h"
 #include <cstdio>
 #include <cstring>
 #include <string>
