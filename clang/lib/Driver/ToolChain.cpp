dcad8b2425cd2dc5ede627d1365e94f5b8682318
John F. Carr <jfc@mit.edu>
2020-03-15 15:54:47 -0400

    New ABI

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -1325,6 +1325,7 @@ void ToolChain::AddTapirRuntimeLibArgs(const ArgList &Args,
 
   switch (TapirTarget) {
   case TapirTargetID::Cheetah:
+  case TapirTargetID::OpenCilk:
     CmdArgs.push_back("-lcheetah");
     CmdArgs.push_back("-lpthread");
     break;
