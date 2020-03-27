948c0079a60c48d4dad86324784783a2c37cf07a
Patrick McCormick <pat@lanl.gov>
2020-03-27 14:41:08 -0600

    Patching some command line building/additions for Kitsune/Tapri specific needs.

--- clang/lib/StaticAnalyzer/Core/ExprEngine.cpp
+++ clang/lib/StaticAnalyzer/Core/ExprEngine.cpp
@@ -1232,6 +1232,8 @@ void ExprEngine::Visit(const Stmt *S, ExplodedNode *Pred,
     case Stmt::CilkSpawnStmtClass:
     case Expr::CilkSpawnExprClass:
     case Stmt::CilkSyncStmtClass:
+    case Stmt::SpawnStmtClass:
+    case Stmt::SyncStmtClass:      
     case Stmt::CoroutineBodyStmtClass:
     case Stmt::CoawaitExprClass:
     case Stmt::DependentCoawaitExprClass:
@@ -1332,6 +1334,7 @@ void ExprEngine::Visit(const Stmt *S, ExplodedNode *Pred,
       // analyzer can reconstruct these values from the AST.
       llvm_unreachable("Should be pruned from CFG");
     case Stmt::ForallStmtClass:
+    case Stmt::CXXForallRangeStmtClass:
       llvm_unreachable("Stmt should not be in analyzer evaluation loop");
 
     case Stmt::ObjCSubscriptRefExprClass:
