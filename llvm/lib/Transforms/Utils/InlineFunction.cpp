fe2e59bb7b8aca35de10ad4789dab38c348b5dba
TB Schardl <neboat@mit.edu>
2020-04-24 14:13:25 +0000

    [InlineFunction] Fix bug to handle inlined resumes in nested taskframes.

--- llvm/lib/Transforms/Utils/InlineFunction.cpp
+++ llvm/lib/Transforms/Utils/InlineFunction.cpp
@@ -2328,6 +2328,7 @@ static void HandleInlinedResumeInTask(BasicBlock *EntryBlock, BasicBlock *Ctx,
       return;
     }
 
+
     // Insert an invocation of detached.rethrow before the resume.
     BasicBlock *NewBB = SplitResume(Resume, Intrinsic::detached_rethrow,
                                     SyncRegion, Unreachable);
