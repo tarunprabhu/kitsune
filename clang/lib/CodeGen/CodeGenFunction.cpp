f8abb4a53e5b1ced1c8589ec5daf1566c0892a8a
TB Schardl <neboat@mit.edu>
2020-05-01 15:08:07 +0000

    [CodeGenFunction] Clean up merged code for inserting an implicit sync at the end of a function.

--- clang/lib/CodeGen/CodeGenFunction.cpp
+++ clang/lib/CodeGen/CodeGenFunction.cpp
@@ -366,6 +366,7 @@ void CodeGenFunction::FinishFunction(SourceLocation EndLoc) {
 
   std::optional<ApplyDebugLocation> OAL;
   bool SyncEmitted = false;
+  bool CompilingCilk = (getLangOpts().getCilk() != LangOptions::Cilk_none);
   if (HasCleanups) {
     // Make sure the line table doesn't jump back into the body for
     // the ret after it's been at EndLoc.
@@ -378,9 +379,9 @@ void CodeGenFunction::FinishFunction(SourceLocation EndLoc) {
         OAL = ApplyDebugLocation::CreateDefaultArtificial(*this, EndLoc);
     }
 
-    PopCleanupBlocks(PrologueCleanupDepth, {}, getLangOpts().Cilk);
+    PopCleanupBlocks(PrologueCleanupDepth, {}, CompilingCilk);
     SyncEmitted = true;
-  } else if (getLangOpts().Cilk && Builder.GetInsertBlock()) {
+  } else if (CompilingCilk && Builder.GetInsertBlock()) {
     // If we're compiling Cilk, emit an implicit sync for the function.
     EmitImplicitSyncCleanup();
     SyncEmitted = true;
@@ -389,7 +390,7 @@ void CodeGenFunction::FinishFunction(SourceLocation EndLoc) {
   // Emit function epilog (to return).
   llvm::DebugLoc Loc = EmitReturnBlock();
 
-  if (getLangOpts().Cilk && !SyncEmitted) {
+  if (CompilingCilk && !SyncEmitted) {
     // If we're compiling Cilk, emit an implicit sync for the function.
     EmitImplicitSyncCleanup();
     SyncEmitted = true;
