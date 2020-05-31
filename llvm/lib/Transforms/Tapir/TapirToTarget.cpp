a7ef6c50538af9170bc87d6581450bf5c90c1d74
George Stelle <stelleg@lanl.gov>
2020-05-31 09:56:06 -0600

    Fixed TLI handling

--- llvm/lib/Transforms/Tapir/TapirToTarget.cpp
+++ llvm/lib/Transforms/Tapir/TapirToTarget.cpp
@@ -534,6 +534,7 @@ PreservedAnalyses TapirToTargetPass::run(Module &M, ModuleAnalysisManager &AM) {
 
   if (Changed)
     return PreservedAnalyses::none();
+
   return PreservedAnalyses::all();
 }
 
