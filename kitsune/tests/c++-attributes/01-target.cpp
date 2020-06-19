54a3373c5e515ae54fa1ccc91d1526ed686f121b
Patrick McCormick <pat@lanl.gov>
2020-06-19 15:41:43 -0600

    Fixed race in kokkos parallel_for codegen, refactored and cleaned up a bit of the kokkos codegen implementation, and cleaned up some error and warnings.
    Some work on command line arguments and details -- still needs some more work and
    some connection between build settings and default targets.

---
+++ kitsune/tests/c++-attributes/01-target.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <kitsune.h>
+
+
+const unsigned int N = 4;
+
+int main(int argc, char *argv[])
+{
+  [[tapir::rt_target("qthreads")]]
+  forall(int i = 0; i < N; i++) {
+    cout << "i : " << i << endl;
+  }
+
+  return 0;
+}
+
