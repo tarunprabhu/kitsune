412a494cf9443ccea8886f39b8b7c5d557553044
George Stelle <stelleg@lanl.gov>
2020-08-13 10:06:35 -0600

    Working spirv kernel codegen

--- llvm/lib/Transforms/Utils/TapirUtils.cpp
+++ llvm/lib/Transforms/Utils/TapirUtils.cpp
@@ -2309,6 +2309,7 @@ void llvm::TapirLoopHints::clearHintsMetadata() {
 bool llvm::hintsDemandOutlining(const TapirLoopHints &Hints) {
   switch (Hints.getStrategy()) {
   case TapirLoopHints::ST_DAC: return true;
+  case TapirLoopHints::ST_OCL: return true;
   default: return false;
   }
 }
