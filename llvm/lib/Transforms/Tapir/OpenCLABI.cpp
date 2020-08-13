1232f6c0ca0178a7cd26dd0900feca28111afae1
George Stelle <stelleg@lanl.gov>
2020-08-13 10:06:35 -0600

    Working spirv kernel codegen

--- llvm/lib/Transforms/Tapir/OpenCLABI.cpp
+++ llvm/lib/Transforms/Tapir/OpenCLABI.cpp
@@ -13,34 +13,59 @@
 #include "llvm/Transforms/Tapir/LoopSpawningTI.h"
 #include "llvm/Transforms/Tapir/OpenCLABI.h"
 #include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/IR/IntrinsicInst.h"
 #include <LLVMSPIRVLib/LLVMSPIRVLib.h>
 #include <sstream>
 
 using namespace llvm;
 
 /// The opencl loop-outline processor leaves the outlined Tapir loop as is.
-  void OCLSpawning::postProcessOutline(TapirLoopInfo &TL, TaskOutlineInfo &Out,
-                          ValueToValueMapTy &VMap) {
-    LoopOutlineProcessor::postProcessOutline(TL, Out, VMap);
-    Module CLM(M.getModuleIdentifier() + "_CL", M.getContext()); 
-    Function *OL = Out.Outline; 
-    Function *CLF = Function::Create(OL->getFunctionType(), OL->getLinkage(), OL->getName()); 
-    CLM.getFunctionList().push_back(CLF);
-    CLM.getOrInsertFunction(CLF->getName(), CLF->getFunctionType(),
-                          CLF->getAttributes());
-    SmallVector<ReturnInst*, 8> Returns;
-    CloneFunctionInto(OL, CLF, VMap, /*ModuleLevelChanges=*/false, Returns);
-    Out.Outline = CLF; 
-    
-    std::ostringstream str; 
-    std::string ErrMsg; 
-    bool Fail = writeSpirv(&CLM, str, ErrMsg); 
-    assert(!Fail && "Failed to emit spirv");
+void OCLSpawning::postProcessOutline(TapirLoopInfo &TL, TaskOutlineInfo &Out,
+                        ValueToValueMapTy &VMap) {
+  LoopOutlineProcessor::postProcessOutline(TL, Out, VMap);
+  Module CLM(M.getModuleIdentifier() + "_CL", M.getContext()); 
 
-    // Create a global string to hold the PTX code
-    Constant *SPIRV = ConstantDataArray::getString(M.getContext(), str.str());
-    GlobalVariable(M, SPIRV->getType(), true,
-                                   GlobalValue::PrivateLinkage, SPIRV,
-                                   "spirv_" + Twine(OL->getName()));
+  CLM.setTargetTriple("spir64-unknown-unknown");
+
+  Function *OL = Out.Outline; 
+  Function *CLF = Function::Create(OL->getFunctionType(), OL->getLinkage(), OL->getName()); 
+  CLM.getFunctionList().push_back(CLF);
+  CLM.getOrInsertFunction(CLF->getName(), CLF->getFunctionType(),
+                          CLF->getAttributes());
+  SmallVector<ReturnInst*, 8> Returns;
+  ValueToValueMapTy spirv_vmap;
+  auto spirvArg = CLF->arg_begin();
+  for(auto &arg : OL->args()) 
+    spirv_vmap[&arg] = &*spirvArg++;
+
+
+  CloneFunctionInto(CLF, OL, spirv_vmap, false, Returns);
+  for(BasicBlock &BB : *CLF){
+    for(Instruction &I : BB){
+      IntrinsicInst *II = dyn_cast<IntrinsicInst>(&I);
+      if(II){
+        if(II->getIntrinsicID() == Intrinsic::syncregion_start){
+          II->eraseFromParent(); 
+          break; 
+        }
+      }
+    }
+  }
+  // TODO: Handle all calls into functions
+
+  
+  std::ostringstream str; 
+  std::string ErrMsg; 
+  bool success = writeSpirv(&CLM, str, ErrMsg); 
+  if(!success){
+    std::cerr << "Failed to compile to spirv: " << ErrMsg << std::endl; 
+    exit(1); 
+  }
+
+  // Create a global string to hold the PTX code
+  Constant *SPIRV = ConstantDataArray::getString(M.getContext(), str.str());
+  SPIRVKernel = new GlobalVariable(M, SPIRV->getType(), true,
+                                 GlobalValue::PrivateLinkage, SPIRV,
+                                 "spirv_" + Twine(OL->getName()));
 }
 
