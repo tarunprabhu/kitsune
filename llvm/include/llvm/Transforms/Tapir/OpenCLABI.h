8072257f904d38e7f16ab6112eb17f63e5a43fc2
George Stelle <stelleg@lanl.gov>
2020-09-01 12:09:06 -0600

    Basic outline for OpenCL tapir target, replacing loop hint

--- llvm/include/llvm/Transforms/Tapir/OpenCLABI.h
+++ llvm/include/llvm/Transforms/Tapir/OpenCLABI.h
@@ -1,3 +1,89 @@
+//===- OpenCLABI.h - Interface to the Kitsune OpenCL back end ------*- C++ -*--===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the Kitsune OpenCL ABI to convert Tapir instructions to
+// calls into the Kitsune runtime system for NVIDIA GPU code.
+//
+//===----------------------------------------------------------------------===//
+#ifndef OpenCL_ABI_H_
+#define OpenCL_ABI_H_
+
+#include "llvm/Transforms/Tapir/LoweringUtils.h"
+#include "llvm/Transforms/Tapir/TapirLoopInfo.h"
+
+namespace llvm {
+
+class DataLayout;
+class TargetMachine;
+
+class OpenCLABI : public TapirTarget {
+public:
+  OpenCLABI(Module &M) : TapirTarget(M) {}
+  ~OpenCLABI() {}
+  Value *lowerGrainsizeCall(CallInst *GrainsizeCall) override final;
+  void lowerSync(SyncInst &SI) override final;
+
+  void addHelperAttributes(Function &F) override final {}
+  void preProcessFunction(Function &F, TaskInfo &TI,
+                          bool OutliningTapirLoops) override final;
+  void postProcessFunction(Function &F, bool OutliningTapirLoops)
+    override final;
+  void postProcessHelper(Function &F) override final;
+
+  void processOutlinedTask(Function &F) override final;
+  void processSpawner(Function &F) override final;
+  void processSubTaskCall(TaskOutlineInfo &TOI, DominatorTree &DT)
+    override final;
+
+  LoopOutlineProcessor *getLoopOutlineProcessor(const TapirLoopInfo *TL) const
+    override final;
+};
+
+class SPIRVLoop : public LoopOutlineProcessor {
+private:
+  static unsigned NextKernelID;
+  unsigned MyKernelID;
+  Module SPIRVM;
+  TargetMachine *SPIRVTargetMachine;
+  GlobalVariable *SPIRVGlobal;
+
+  FunctionCallee GetThreadIdx = nullptr;
+  FunctionCallee GetBlockIdx = nullptr;
+  FunctionCallee GetBlockDim = nullptr;
+  FunctionCallee KitsuneOpenCLInit = nullptr;
+  FunctionCallee KitsuneGPUInitKernel = nullptr;
+  FunctionCallee KitsuneGPUInitField = nullptr;
+  FunctionCallee KitsuneGPUSetRunSize = nullptr;
+  FunctionCallee KitsuneGPURunKernel = nullptr;
+  FunctionCallee KitsuneGPUFinish = nullptr;
+public:
+  SPIRVLoop(Module &M);
+
+  void setupLoopOutlineArgs(
+      Function &F, ValueSet &HelperArgs, SmallVectorImpl<Value *> &HelperInputs,
+      ValueSet &InputSet, const SmallVectorImpl<Value *> &LCArgs,
+      const SmallVectorImpl<Value *> &LCInputs,
+      const ValueSet &TLInputsFixed)
+    override final;
+  unsigned getIVArgIndex(const Function &F, const ValueSet &Args) const
+    override final;
+  unsigned getLimitArgIndex(const Function &F, const ValueSet &Args) const
+    override final;
+  void postProcessOutline(TapirLoopInfo &TL, TaskOutlineInfo &Out,
+                          ValueToValueMapTy &VMap) override final;
+  void processOutlinedLoopCall(TapirLoopInfo &TL, TaskOutlineInfo &TOI,
+                               DominatorTree &DT) override final;
+};
+}
+
+#endif
+/*
 #include "llvm/Transforms/Tapir/LoopSpawningTI.h"
 #include "llvm/Transforms/Tapir/LoweringUtils.h"
 #include "llvm/ADT/DenseMap.h"
@@ -11,3 +97,4 @@ public:
                           ValueToValueMapTy &VMap) override final;  
   GlobalVariable* SPIRVKernel; 
 };
+*/
--- llvm/lib/Transforms/Tapir/LoweringUtils.cpp
+++ llvm/lib/Transforms/Tapir/LoweringUtils.cpp
@@ -21,6 +21,7 @@
 #include "llvm/Transforms/Tapir/CudaABI.h"
 #include "llvm/Transforms/Tapir/OpenCilkABI.h"
 #include "llvm/Transforms/Tapir/OpenMPABI.h"
+#include "llvm/Transforms/Tapir/OpenCLABI.h"
 #include "llvm/Transforms/Tapir/Outline.h"
 #include "llvm/Transforms/Tapir/QthreadsABI.h"
 #include "llvm/Transforms/Tapir/SerialABI.h"
@@ -49,6 +50,8 @@ TapirTarget *llvm::getTapirTargetFromID(Module &M, TapirTargetID ID) {
   case TapirTargetID::Cheetah:
   case TapirTargetID::OpenCilk:
     return new OpenCilkABI(M);
+  case TapirTargetID::OpenCL:
+    return new OpenCLABI(M);
   case TapirTargetID::OpenMP:
     return new OpenMPABI(M);
   case TapirTargetID::Qthreads:
