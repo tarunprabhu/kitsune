f956206ba72fea74745d6fc6108e49a1ca283ed3
TB Schardl <neboat@mit.edu>
2020-09-03 02:39:41 +0000

    Bug fixes for rebase onto LLVM 10.0.1

--- clang/lib/Parse/ParseExpr.cpp
+++ clang/lib/Parse/ParseExpr.cpp
@@ -1776,7 +1776,7 @@ ExprResult Parser::ParseCastExpression(CastParseKind ParseKind,
     //   return ExprError();
     // }
 
-    Res = ParseCastExpression(false);
+    Res = ParseCastExpression(AnyCastExpr);
     if (!Res.isInvalid())
       Res = Actions.ActOnCilkSpawnExpr(SpawnLoc, Res.get());
     return Res;
