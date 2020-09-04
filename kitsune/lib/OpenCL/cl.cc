0e40d76f585c0e60f1f0bcfc7d7acc6e176a8433
George Stelle <stelleg@lanl.gov>
2020-09-03 22:31:50 -0600

    Added kitsune opencl calls for codegen, still need variable ordering and memory copies

--- kitsune/lib/OpenCL/cl.cc
+++ kitsune/lib/OpenCL/cl.cc
@@ -1,7 +1,13 @@
 #include <CL/cl.hpp>
 #include<iostream>
+#include<stdint.h>
 
-extern "C" void _kitsune_cl_call(void* spirkernel, size_t len, size_t n); 
+extern "C" void __kitsune_opencl_init(); 
+extern "C" void __kitsune_opencl_init_kernel(size_t id, size_t len, void* spirkernel); 
+extern "C" void __kitsune_opencl_set_arg(int id, int argid, void* arg, uint32_t size, uint8_t mode); 
+extern "C" void __kitsune_opencl_set_run_size(int id, uint64_t n); 
+extern "C" void __kitsune_opencl_run_kernel(int id); 
+extern "C" void __kitsune_opencl_finish(); 
 
 using namespace std; 
 
@@ -12,18 +18,48 @@ void check(bool pred, std::string msg){
   }
 }
 
-void run_kernel(void* spirkernel, size_t len, size_t n){
-  std::vector<cl::Platform> platforms;
-  cl::Platform::get(&platforms); 
-  check(platforms.size() > 0, "No opencl platforms found\n");
-  std::vector<cl::Device> devices; 
-  platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
-  check(devices.size() > 0, "No opencl gpu devices found\n"); 
-  cl::Context context{devices[0]};
-  auto commandQueue  = cl::CommandQueue{context, devices[0]};
-  cl::Program program{clCreateProgramWithIL(context(), spirkernel, 1, nullptr)};   
+/* Globals */
+bool initialized = false; 
+std::vector<cl::Platform> platforms;
+std::vector<cl::Device> devices; 
+cl::Kernel kernels[1024]; 
+cl::Context context; 
+cl::CommandQueue commandQueue; 
+uint64_t sizes[1024]; 
+
+void __kitsune_opencl_init(){
+  if(!initialized){
+    cl::Platform::get(&platforms); 
+    check(platforms.size() > 0, "No opencl platforms found\n");
+    platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
+    check(devices.size() > 0, "No opencl gpu devices found\n"); 
+    cl::Context c{devices[0]};
+    context = c; 
+    commandQueue = cl::CommandQueue{context, devices[0]};
+    initialized = true; 
+  }
+}
+
+void __kitsune_opencl_init_kernel(size_t id, size_t len, void* spirkernel){
+  cl_int err; 
+  cl::Program program {clCreateProgramWithIL(context(), spirkernel, len, nullptr)};   
   program.build(nullptr); 
-  auto kernel = cl::Kernel{program, "kitsune_spirv_kernel"}; 
-  commandQueue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange{n}); 
+  kernels[id] = cl::Kernel{program, "kitsune_spirv_kernel"};
+}
+
+void __kitsune_opencl_set_arg(int id, int argid, void* arg, uint32_t size, uint8_t mode){
+  clSetKernelArg(kernels[id](), argid, size, arg); 
+}
+
+void __kitsune_opencl_set_run_size(int id, uint64_t n){
+  sizes[id] = n; 
+}
+
+void __kitsune_opencl_run_kernel(int id){
+  commandQueue.enqueueNDRangeKernel(kernels[id], cl::NullRange, cl::NDRange{sizes[id]}); 
   commandQueue.finish(); 
+}
+
+void __kitsune_opencl_finish(){
+  // TODO
 }
