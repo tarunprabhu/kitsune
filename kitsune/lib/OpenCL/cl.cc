31edcc58523ba1b3d32ca093f8464228eb9e37b5
George Stelle <stelleg@lanl.gov>
2020-09-04 10:28:57 -0600

    Added buffer creation with fixed read/write detection

--- kitsune/lib/OpenCL/cl.cc
+++ kitsune/lib/OpenCL/cl.cc
@@ -9,6 +9,8 @@ extern "C" void __kitsune_opencl_set_run_size(int id, uint64_t n);
 extern "C" void __kitsune_opencl_run_kernel(int id); 
 extern "C" void __kitsune_opencl_finish(); 
 
+extern "C" void* __kitsune_opencl_mem_move(int id, void* arg, uint64_t size, uint8_t mode); 
+
 using namespace std; 
 
 void check(bool pred, std::string msg){
@@ -38,6 +40,19 @@ void __kitsune_opencl_init(){
     commandQueue = cl::CommandQueue{context, devices[0]};
     initialized = true; 
   }
+}
+
+void* __kitsune_opencl_mem_move(size_t id, void* ptr, uint64_t size, uint8_t mode){
+  cl_int err; 
+  cl_mem_flags mf = 
+    mode & 1 && mode & 2 ? CL_MEM_READ_WRITE :
+    mode & 1 ? CL_MEM_READ_ONLY : 
+    mode & 2 ? CL_MEM_WRITE_ONLY :
+    CL_MEM_READ_WRITE; 
+
+  cl_mem mem = clCreateBuffer(context(), mf, (size_t)size, ptr, &err);  
+  check(err == CL_SUCCESS, "clCreateBuffer");
+  return (void*) mem; 
 }
 
 void __kitsune_opencl_init_kernel(size_t id, size_t len, void* spirkernel){
