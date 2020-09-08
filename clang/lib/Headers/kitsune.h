37047ef20bb9d3f7ff66941221721fdca14e2ab8
George Stelle <stelleg@lanl.gov>
2020-09-08 16:06:53 -0600

    Fixed ocl_mmap marker

--- clang/lib/Headers/kitsune.h
+++ clang/lib/Headers/kitsune.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 #define ocl_mmap(a, n) __kitsune_opencl_mmap_marker((void*)a, n)
-void* __kitsune_opencl_mmap_marker(void* ptr, uint64_t n);
+void __kitsune_opencl_mmap_marker(void* ptr, uint64_t n);
 #define spawn _kitsune_spawn
 #define sync _kitsune_sync
 #define forall _kitsune_forall
