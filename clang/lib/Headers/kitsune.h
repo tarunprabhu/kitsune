d8cb24ff895624f469d0cffeeb14cadd3981a606
George Stelle <stelleg@lanl.gov>
2020-09-07 23:10:59 -0600

    Working simple example with ocl_mmap, should write to and read from device

--- clang/lib/Headers/kitsune.h
+++ clang/lib/Headers/kitsune.h
@@ -1,10 +1,11 @@
 #ifndef __KITSUNE__H__
 #define __KITSUNE__H__
+#include <stdint.h>
 
+#define ocl_mmap(a, n) __kitsune_opencl_mmap_marker((void*)a, n)
+void* __kitsune_opencl_mmap_marker(void* ptr, uint64_t n);
 #define spawn _kitsune_spawn
 #define sync _kitsune_sync
 #define forall _kitsune_forall
 
 #endif
-
-
