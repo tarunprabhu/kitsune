ae9c7dde14ede742967fe7938056c9348b12e867
George Stelle <stelleg@lanl.gov>
2020-09-09 15:33:08 -0600

    Small spirv fixes

--- llvm/lib/Transforms/Tapir/OpenCLABI.cpp
+++ llvm/lib/Transforms/Tapir/OpenCLABI.cpp
@@ -81,12 +81,12 @@ SPIRVLoop::SPIRVLoop(Module &M)
 
   // Insert runtime-function declarations in SPIRV host modules.
   Type *SPIRVInt32Ty = Type::getInt32Ty(SPIRVM.getContext());
-  GetThreadIdx = SPIRVM.getOrInsertFunction("get_global_id",
+  GetThreadIdx = SPIRVM.getOrInsertFunction("_Z13get_global_idj", // not sure why this mangled name is the key, but it is
                                           SPIRVInt32Ty, SPIRVInt32Ty);
-  GetBlockIdx = SPIRVM.getOrInsertFunction("get_local_id",
-                                         SPIRVInt32Ty, SPIRVInt32Ty);
-  GetBlockDim = SPIRVM.getOrInsertFunction("get_local_size",
-                                         SPIRVInt32Ty, SPIRVInt32Ty);
+  //GetBlockIdx = SPIRVM.getOrInsertFunction("get_local_id",
+   //                                      SPIRVInt32Ty, SPIRVInt32Ty);
+  //GetBlockDim = SPIRVM.getOrInsertFunction("get_local_size",
+  //                                      SPIRVInt32Ty, SPIRVInt32Ty);
 
   Type *VoidTy = Type::getVoidTy(M.getContext());
   Type *VoidPtrTy = Type::getInt8PtrTy(M.getContext());
@@ -197,15 +197,14 @@ void SPIRVLoop::postProcessOutline(TapirLoopInfo &TL, TaskOutlineInfo &Out,
 
   // Set the helper function to have external linkage.
   Helper->setLinkage(Function::ExternalLinkage);
+  Helper->setName("kitsune_spirv_kernel"); 
 
   // Get the thread ID for this invocation of Helper.
   IRBuilder<> B(Entry->getTerminator());
   Value *ThreadIdx = B.CreateCall(GetThreadIdx, ConstantInt::get(Int32Ty, 0));
-  Value *BlockIdx = B.CreateCall(GetBlockIdx, ConstantInt::get(Int32Ty, 0));
-  Value *BlockDim = B.CreateCall(GetBlockDim, ConstantInt::get(Int32Ty, 0));
-  Value *ThreadID = B.CreateIntCast(
-      B.CreateAdd(ThreadIdx, B.CreateMul(BlockIdx, BlockDim), "threadId"),
-      PrimaryIV->getType(), false);
+  //Value *BlockIdx = B.CreateCall(GetBlockIdx, ConstantInt::get(Int32Ty, 0));
+  //Value *BlockDim = B.CreateCall(GetBlockDim, ConstantInt::get(Int32Ty, 0));
+  Value *ThreadID = B.CreateIntCast(ThreadIdx, PrimaryIV->getType(), false);
 
   // Verify that the Thread ID corresponds to a valid iteration.  Because Tapir
   // loops use canonical induction variables, valid iterations range from 0 to
@@ -393,116 +392,4 @@ void SPIRVLoop::processOutlinedLoopCall(TapirLoopInfo &TL, TaskOutlineInfo &TOI,
 
   ReplCall->eraseFromParent();
 }
-
-/*
-#include "llvm/Transforms/Tapir/TapirLoopInfo.h"
-#include "llvm/Transforms/Tapir/LoopSpawningTI.h"
-#include "llvm/Transforms/Tapir/OpenCLABI.h"
-#include "llvm/Transforms/Utils/Cloning.h"
-#include "llvm/Transforms/Utils/BasicBlockUtils.h"
-#include "llvm/IR/IntrinsicInst.h"
-#include <LLVMSPIRVLib/LLVMSPIRVLib.h>
-#include <sstream>
-
-using namespace llvm;
-
-void RuntimeCilkFor::processOutlinedLoopCall(TapirLoopInfo &TL,
-                                             TaskOutlineInfo &TOI,
-                                             DominatorTree &DT) {
-  Function *Outlined = TOI.Outline;
-  Instruction *ReplStart = TOI.ReplStart;
-  Instruction *ReplCall = TOI.ReplCall;
-  CallSite CS(ReplCall);
-  BasicBlock *CallCont = TOI.ReplRet;
-  BasicBlock *UnwindDest = TOI.ReplUnwind;
-  Function *Parent = ReplCall->getFunction();
-  Module &M = *Parent->getParent();
-  unsigned IVArgIndex = getIVArgIndex(*Parent, TOI.InputSet);
-  Type *PrimaryIVTy =
-      CS.getArgOperand(IVArgIndex)->getType();
-  Value *TripCount = CS.getArgOperand(IVArgIndex + 1);
-  Value *GrainsizeVal = CS.getArgOperand(IVArgIndex + 2);
-
-  // TODO: calls into external functions
-  // TODO: captured values
-  
-  // Types
-  LLVMContext &Ctx = M.getContext();
-  Type *VoidTy = Type::getVoidTy(M.getContext());
-  Type *Int8Ty = Type::getInt8Ty(Ctx);
-  Type *Int32Ty = Type::getInt32Ty(Ctx);
-  Type *Int64Ty = Type::getInt64Ty(Ctx);
-  Type *VoidPtrTy = Type::getInt8PtrTy(Ctx);
-  IRBuilder<> B(Out.ReplCall); 
-
-  // spirv function
-  Function *OL = Out.Outline; 
-  Function *CLF = Function::Create(OL->getFunctionType(), OL->getLinkage(), OL->getName()); 
-  CLM.getFunctionList().push_back(CLF);
-  CLM.getOrInsertFunction(CLF->getName(), CLF->getFunctionType(),
-                          CLF->getAttributes());
-  
-
-void OpenCL::postProcessOutline(TapirLoopInfo &TL, TaskOutlineInfo &Out,
-                                 ValueToValueMapTy &VMap) {
-  
-  // spirv module
-  Module CLM(M.getModuleIdentifier() + "_SPIRV", M.getContext()); 
-  CLM.setTargetTriple("spir64-unknown-unknown");
-
-  // spirv function
-  Function *OL = Out.Outline; 
-  Function *CLF = Function::Create(OL->getFunctionType(), OL->getLinkage(), OL->getName()); 
-  CLM.getFunctionList().push_back(CLF);
-  CLM.getOrInsertFunction(CLF->getName(), CLF->getFunctionType(),
-                          CLF->getAttributes());
-  SmallVector<ReturnInst*, 8> Returns;
-  ValueToValueMapTy spirv_vmap;
-  auto spirvArg = CLF->arg_begin();
-  for(auto &arg : OL->args()) 
-    spirv_vmap[&arg] = &*spirvArg++;
-  
-  CloneFunctionInto(CLF, OL, spirv_vmap, false, Returns); 
-  for(BasicBlock &BB : *CLF){
-    for(Instruction &I : BB){
-      IntrinsicInst *II = dyn_cast<IntrinsicInst>(&I);
-      if(II){
-        if(II->getIntrinsicID() == Intrinsic::syncregion_start){
-          II->eraseFromParent(); 
-          break; 
-        }
-      }
-    }
-  }
-
-  // remove old outlined loop
-  // OL->eraseFromParent();  // causes issues with later hint removal pass
-
-  // generate spirv kernel code
-  std::ostringstream str; 
-  std::string ErrMsg; 
-  bool success = writeSpirv(&CLM, str, ErrMsg); 
-  if(!success){
-    std::cerr << "Failed to compile to spirv: " << ErrMsg << std::endl; 
-    exit(1); 
-  }
-  auto s = str.str(); 
-  Constant *SPIRV = ConstantDataArray::getRaw(s, s.length(), Int8Ty);
-  SPIRVKernel = new GlobalVariable(M, SPIRV->getType(), true,
-                                 GlobalValue::PrivateLinkage, SPIRV,
-                                 "spirv_" + Twine(OL->getName()));
-  Value *SPIRVPtr = B.CreateBitCast(SPIRVKernel, VoidPtrTy);
-  Constant *kernelSize = ConstantInt::get(Int32Ty, 
-    SPIRVKernel->getInitializer()->getType()->getArrayNumElements()); 
-
-  // insert runtime call
-  auto KitsuneOpenCLCall = M.getOrInsertFunction("_kitsune_cl_call", VoidTy, VoidPtrTy, Int32Ty, Int64Ty);
-  auto NewReplCall = B.CreateCall(KitsuneOpenCLCall, { SPIRVPtr, kernelSize, TL.getTripCount() }); 
-  Loop *L = TL.getLoop(); 
-  //BasicBlock *Entry = L->getLoopPreheader();
-  //BasicBlock *Exit = TL.getExitBlock();
-  //ReplaceInstWithInst(Entry->getTerminator(), BranchInst::Create(Exit));
-  Out.replaceReplCall(NewReplCall); 
-}
-*/
 
