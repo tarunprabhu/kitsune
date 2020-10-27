02735ce493595a4ca20153989a69273c90857b42
Alexis Perry-Holby <aperry@lanl.gov>
2020-10-27 16:44:29 -0600

    Added necessary build things and initial commit of RealmABI stuff   - Re-plumbed the -ftapir=realm option   - Re-plumbed the kitsune-rt build   - initial commit of RealmABI.h and RealmABI.cpp, no changes from realm_9.x branch

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -3698,6 +3698,8 @@ bool CompilerInvocation::CreateFromArgs(CompilerInvocation &Res,
       LangOpts.Tapir = TapirTargetID::OpenMP;
     else if (Name == "qthreads")
       LangOpts.Tapir = TapirTargetID::Qthreads;
+    else if (Name == "realm")
+      LangOpts.Tapir = TapirTargetID::Realm;
     else if (Name == "cuda")
       LangOpts.Tapir = TapirTargetID::Cuda;
     else if (Name == "kitcuda")
