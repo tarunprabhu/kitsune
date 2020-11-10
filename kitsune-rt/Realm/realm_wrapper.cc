a94c6f59196c9dee1026673c088d8d709dcfb084
Alexis Perry-Holby <aperry@lanl.gov>
2020-11-09 17:05:35 -0700

    DOES NOT BUILD - initial add of George Stelle's code for using Realm::Barriers

--- kitsune-rt/Realm/realm_wrapper.cc
+++ kitsune-rt/Realm/realm_wrapper.cc
@@ -28,6 +28,15 @@ extern "C" {
       return NULL;
   }
   
+  Realm::Barrier createRealmBarrier(){
+    return Realm::Barrier::create_barrier(1);
+  }
+
+  void destroyRealmBarrier(Realm::Barrier b){
+    b.destroy_barrier();  
+  }
+
+  
   int realmInitRuntime(int argc, char** argv) {
     std::cout << "realmInitRuntime" << std::endl;
     if (initCalled)
@@ -49,8 +58,7 @@ extern "C" {
     for(auto it = locprocquery.begin(); it != locprocquery.end(); it++)
       _globalCTX->procs.push_back(*it);
 
-    Realm::Processor first_proc = _globalCTX->procs[0];
-    _globalCTX->procgroup = first_proc.create_group(_globalCTX->procs);
+    _globalCTX->procgroup = Realm::ProcessorGroup::create_group(_globalCTX->procs);
 
     //_globalCTX->cur_task = Realm::Processor::TASK_ID_FIRST_AVAILABLE;
     (_globalCTX->cur_task).store(Realm::Processor::TASK_ID_FIRST_AVAILABLE);
@@ -66,7 +74,11 @@ extern "C" {
     else
       return 0;
   }
-  
+    
+  void realmArrive(Realm::Barrier b){
+    b.arrive(); 
+  }
+
   void realmSpawn(Realm::Processor::TaskFuncPtr func, const void* args, size_t arglen, void* user_data, size_t user_data_len);
 
   void realmSpawn(Realm::Processor::TaskFuncPtr func, 
@@ -92,17 +104,30 @@ extern "C" {
     //get a processor to run on
     Realm::Processor p = ctx->procgroup; //spawn on the group to enable Realm's magic load-balancing
     //Realm::Processor p = (ctx->procs)[i]; //do round-robin spawning on the vector of procs (needs i calculated)
+    std::cout << "    accessed ctx->procgroup" << std::endl;
+    assert(p.exists());
+    assert(p != Realm::Processor::NO_PROC);
 
     // Create a CodeDescriptor from the TaskFuncPtr   
     Realm::CodeDescriptor cd = Realm::CodeDescriptor(func);
+    std::cout << "    got a CodeDescriptor" << std::endl;
 
     const Realm::ProfilingRequestSet prs;  //We don't care what it is for now, the default is fine
+    std::cout << "    got a default ProfilingRequestSet" << std::endl;
 
+    //make sure processor is not busy
+    //Realm::Event done_yet = p.get_current_finish_event();
+    //std::cout << "      got processor current finish event" << std::endl;
+    //done_yet.wait();
+    //std::cout << "      done_yet complete" << std::endl;
+    
     //register the task with the runtime
     Realm::Event e1 = p.register_task(taskID, cd, prs, user_data, user_data_len);
     std::cout << "     registered task: " << taskID << std::endl;
     ctx->events.insert(e1); //might not actually need to keep track of this one
     std::cout << "      inserted e1" << std::endl;
+    //e1.wait();
+    //std::cout << "      e1 complete" << std::endl;
 
     //spawn the task
     Realm::Event e2 = p.spawn(taskID, args, arglen, e1, 0); //predicated on the completion of the task's registration
@@ -111,25 +136,32 @@ extern "C" {
     std::cout << "        inserted e2" << std::endl;
     return;
   }
-  
+
+#if 0 //the old realmSync (non-barrier)  
   int realmSync() {
     std::cout << " realmSync" << std::endl;
     context *ctx = getRealmCTX();
     assert(ctx);
     //create an event that does not trigger until all previous events have triggered
-    Realm::Event e = Realm::Event::NO_EVENT;
+    Realm::Event e;
+
     if (!(ctx->events).empty()) {
-      e = e.merge_events(ctx->events); 
+      e.merge_events(ctx->events); 
+      std::cout << "  merged events" << std::endl;
       //can clear the events in the list now and insert only the sync event
       ctx->events.clear();
+      std::cout << "   cleared events" << std::endl;
       //ctx->events.insert(e);
     }
-    else
+    else {
       std::cout << "ctx->events is empty" << std::endl;
+      e = Realm::Event::NO_EVENT;
+    }      
 
     // Do not return until sync is complete
     e.wait();
     std::cout << "e.wait() has completed" << std::endl;
+    
     //while (!e.has_triggered()) {
     //std::cout << "not done yet" << std::endl;
     //continue;
@@ -137,6 +169,15 @@ extern "C" {
     //std:: cout << "done waiting" << std::endl;
     
     return 0;
+  }
+#endif
+  
+  void realmSync(Realm::Barrier b) {
+    b.arrive(); 
+    if(Realm::Thread::self())
+      b.wait(); 
+    else 
+      b.external_wait(); 
   }
 
   void realmFinalize() {
