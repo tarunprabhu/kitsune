66ecc9b703bad09ca4ea33537a7d0c8e6d87baeb
Patrick McCormick <pat@lanl.gov>
2020-11-11 15:20:21 -0700

    Pushing up TB's patches for improved compile times (via tweaks to the how/when verification of the IR occurs.  Should see improvements in large code bases w/ many functions + parallel constructs (of course).

--- llvm/lib/Transforms/Tapir/TapirToTarget.cpp
+++ llvm/lib/Transforms/Tapir/TapirToTarget.cpp
@@ -532,7 +532,6 @@ PreservedAnalyses TapirToTargetPass::run(Module &M, ModuleAnalysisManager &AM) {
 
   if (Changed)
     return PreservedAnalyses::none();
-
   return PreservedAnalyses::all();
 }
 
