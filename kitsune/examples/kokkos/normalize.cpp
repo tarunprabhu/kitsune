3a43592e1fe7348016318c629283342acc4bad04
pmccormick <pat@lanl.gov>
2020-11-20 21:23:15 -0700

    Cleaned up kokkos examples a bit: added a script to run kokkos (installed) examples, tweaked install location of kokkos examples, and made the code a bit more uniform between the examples.

--- kitsune/examples/kokkos/normalize.cpp
+++ kitsune/examples/kokkos/normalize.cpp
@@ -1,4 +1,15 @@
 // 
+// The normalize example from the Tapir PPOP paper converted 
+// to Kokkos.  To enable kitsune+tapir compilation add the 
+// flags to a standard clang compilation: 
+//
+//    * -fkokkos : enable specialized Kokkos recognition and 
+//                 compilation (lower to Tapir).
+//    * -fkokkos-no-init : disable Kokkos initialization and 
+//                 finalization calls to avoid conflicts with
+//                 target runtime operation. 
+//    * -ftapir=rt-target : the runtime ABI to target. 
+// 
 #include <cstdio>
 #include <cmath>
 #include <kitsune/timer.h>
@@ -22,28 +33,30 @@ __attribute__((const, noinline)) double norm(const double *in, size_t N) {
 }
 
 int main (int argc, char* argv[]) {
-  timer t;  
+
+  fprintf(stderr, "**** kitsune+tapir kokkos example: normalize (tapir paper)\n");
+
   double *in = new double[VEC_SIZE];
   double *out = new double[VEC_SIZE];
 
   random_fill(in, VEC_SIZE);
   
   Kokkos::initialize (argc, argv);
-  double secs = t.seconds();
-  fprintf(stdout, "initialization time: %lf seconds.\n", secs);
-  t.reset();
+  timer t;  
   {
     Kokkos::parallel_for(VEC_SIZE, KOKKOS_LAMBDA(const size_t i) {
       out[i] = in[i] / norm(in, VEC_SIZE);
     });
   }
+  Kokkos::finalize();
   double loop_secs = t.seconds();
-  t.reset();
 
-  fprintf(stdout, "%lf, %lf, %lf, %lf\n", out[0], out[22], out[3000], out[4096]); 
-  fprintf(stdout, "seconds = %lf\n", loop_secs);
+  fprintf(stderr, "(%s) %lf, %lf, %lf, %lf\n", 
+          argv[0], out[0], out[VEC_SIZE/4], out[VEC_SIZE/2], out[VEC_SIZE-1]); 
+  fprintf(stdout, "%lf\n", loop_secs);
+
   delete []in;
   delete []out;
-  Kokkos::finalize();
+
   return 0;
 }
