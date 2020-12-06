11b4dd39e9856714bcd07bfdea774a21a6a748c7
George Stelle <stelleg@lanl.gov>
2020-12-06 09:56:46 -0700

    Added opencl to opt options

--- llvm/lib/Analysis/TargetLibraryInfo.cpp
+++ llvm/lib/Analysis/TargetLibraryInfo.cpp
@@ -46,6 +46,8 @@ static cl::opt<TapirTargetID> ClTapirTarget(
                           "cilk", "Cilk Plus"),
                clEnumValN(TapirTargetID::OpenMP,
                           "openmp", "OpenMP"),
+               clEnumValN(TapirTargetID::OpenCL,
+                          "opencl", "OpenCL"),
                clEnumValN(TapirTargetID::Qthreads,
                           "qthreads", "Qthreads"),
                clEnumValN(TapirTargetID::Realm,
