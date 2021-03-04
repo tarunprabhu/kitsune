a21be37f780e1472e8fe52456b446283e3990e70
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-03-04 12:18:03 -0700

    Updates to build configuration details (including rolling in a new module-based set of mechanisms that can be used).

--- kitsune/cmake/modules/FindQthreads.cmake
+++ kitsune/cmake/modules/FindQthreads.cmake
@@ -1,35 +1,38 @@
 # This module will set the following variables in your project:
-# 
+#
 #  Qthreads_FOUND        -- qthreads headers and libraries were found.
-#  Qthreads_INCLUDE_DIR  -- directory with qthreads header file(s). 
-#  Qthreads_LIBRARY      -- full path to qthreads library. 
-#  Qthreads_LIBRARY_DIR  -- path to where the qthreads library is installed. 
-#  Qthreads_LINK_LIBS    -- set of link libraries (e.g. -lqthreads) 
+#  Qthreads_INCLUDE_DIR  -- directory with qthreads header file(s).
+#  Qthreads_LIBRARY      -- full path to qthreads library.
+#  Qthreads_LIBRARY_DIR  -- path to where the qthreads library is installed.
+#  Qthreads_LINK_LIBS    -- set of link libraries (e.g. -lqthreads)
 
 message(STATUS "Looking for qthreads...")
 
 find_path(Qthreads_INCLUDE_DIR qthread.h
   PATHS /usr/local/include
         /opt/include
-	/opt/local/include
+	      /opt/local/include
+        $ENV{QTHREADS_HOME}/include
 )
 
 find_library(Qthreads_LIBRARY qthread
   PATHS /usr/local/lib64
         /usr/local/lib
         /opt/lib64
-        /opt/lib	
-	/opt/local/lib64
-	/opt/local/lib
+        /opt/lib
+	      /opt/local/lib64
+	      /opt/local/lib
+        $ENV{QTHREADS_HOME}/lib
+        $ENV{QTHREADS_HOME}/lib64
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Qthreads 
+find_package_handle_standard_args(Qthreads
   DEFAULT_MSG
-	Qthreads_INCLUDE_DIR 
+	Qthreads_INCLUDE_DIR
 )
 
-if (Qthreads_FOUND) 
+if (Qthreads_FOUND)
   message(STATUS "Found Qthreads runtime.")
   get_filename_component(Qthreads_LIBRARY_DIR
                          ${Qthreads_LIBRARY}
@@ -41,12 +44,12 @@ if (Qthreads_FOUND)
      CACHE
      STRING "List of libraries needed for Qthreads runtime."
   )
-     
+
   message(STATUS "Qthreads runtime include directory : ${Qthreads_INCLUDE_DIR}")
   message(STATUS "Qthreads runtime library directory : ${Qthreads_LIBRARY_DIR}")
   message(STATUS "Qthreads runtime link libraries    : ${Qthreads_LINK_LIBS}")
 else()
 
   message(WARNING "Qthreads runtime NOT found.")
-  
+
 endif()
--- kitsune/include/kitsune/config.h.cmake
+++ kitsune/include/kitsune/config.h.cmake
@@ -2,7 +2,7 @@
  * Copyright (c) 2020 Triad National Security, LLC
  *                         All rights reserved.
  *
- * This file is part of the kitsune/llvm project.  It is released under 
+ * This file is part of the kitsune/llvm project.  It is released under
  * the LLVM license.
  */
 #ifdef KITSUNE_CONFIG_H
@@ -12,8 +12,8 @@
 
 /* Automatic command line arguments to add when compiling with
  * kokkos-centric support enabled (via -fkokkos).
- */ 
-#cmakedefine01 KITSUNE_ENABLE_KOKKOS 
+ */
+#cmakedefine01 KITSUNE_ENABLE_KOKKOS
 #define KOKKOS_INCLUDE_DIR       "${Kokkos_INCLUDE_DIR}"
 #define KOKKOS_LIBRARY_DIR       "${Kokkos_LIBRARY_DIR}"
 #define KOKKOS_LINK_LIBS         "${Kokkos_LINK_LIBS}"
@@ -21,31 +21,31 @@
 
 /* Automatic command line arguments to add when compiling with
  * the Cilk runtime backend target (via -ftapir=cilk).
- */ 
-#cmakedefine01 KITSUNE_ENABLE_CILKRTS_TARGET 
+ */
+#cmakedefine01 KITSUNE_ENABLE_CILKRTS_TARGET
 #define CILKRTS_LIBRARY_DIR      "${CilkRTS_LIBRARY_DIR}"
 #define CILKRTS_LINK_LIBS        "${CilkRTS_LINK_LIBS}"
 #define CILKRTS_EXTRA_LINK_LIBS  "${CILKRTS_EXTRA_LINK_LIBS}"
 
 /* Automatic command line arguments to add when compiling with
  * the OpenCilk runtime backend target (via -ftapir=opencilk).
- */ 
-#cmakedefine01 KITSUNE_ENABLE_OPENCILK_TARGET 
-#define OPENCILK_LIBRARY_DIR      "${OpenCilk_LIBRARY_DIR}"
-#define OPENCILK_LINK_LIBS        "${OpenCilk_LINK_LIBS}"
-#define OPENCILK_EXTRA_LINK_LIBS  "${OPENCILK_EXTRA_LINK_LIBS}"
+ */
+#cmakedefine01 KITSUNE_ENABLE_OPENCILKRTS_TARGET
+#define OPENCILKRTS_LIBRARY_DIR      "${OpenCilkRTS_LIBRARY_DIR}"
+#define OPENCILKRTS_LINK_LIBS        "${OpenCilkRTS_LINK_LIBS}"
+#define OPENCILKRTS_EXTRA_LINK_LIBS  "${OPENCILK_EXTRA_LINK_LIBS}"
 
 /* Automatic command line arguments to add when compiling with
  * the Qthreads runtime backend target (via -ftapir=qthreads).
- */ 
-#cmakedefine01 KITSUNE_ENABLE_QTHREADS_TARGET   
+ */
+#cmakedefine01 KITSUNE_ENABLE_QTHREADS_TARGET
 #define QTHREADS_LIBRARY_DIR     "${Qthreads_LIBRARY_DIR}"
 #define QTHREADS_LINK_LIBS       "${Qthreads_LINK_LIBS}"
 #define QTHREADS_EXTRA_LINK_LIBS "${QTHREADS_EXTRA_LINK_LIBS}"
 
 /* Automatic command line arguments to add when compiling with
  * the Realm runtime backend target (via -ftapir=realm).
- */ 
+ */
 #cmakedefine01 KITSUNE_ENABLE_REALM_TARGET
 #define KITSUNE_REALM_LIBRARY_DIR        "${Realm_LIBRARY_DIR}"
 #define KITSUNE_REALM_WRAPPER_LIBRARY_DIR  "${Realm_WRAPPER_LIBRARY_DIR}"
@@ -53,29 +53,29 @@
 
 /* Automatic command line arguments to add when compiling with
  * the OpenMP runtime backend target (via -ftapir=omp).
- */ 
+ */
 #cmakedefine01 KITSUNE_ENABLE_OPENMP_TARGET
 #define OPENMP_LINK_LIBS          "-lomp"
 
 /* Automatic command line arguments to add when compiling with
  * the CUDA/PTX runtime backend target (via -ftapir=cuda).
- */ 
+ */
 #cmakedefine01 KITSUNE_ENABLE_CUDA_TARGET
 #define CUDA_LIBRARY_DIR         "${CUDAToolkit_LIBRARY_DIR}"
 #define CUDA_LINK_LIBS           "${CUDAToolkit_LINK_LIBS}"
 #define CUDA_EXTRA_LINK_LIBS     "${CUDA_EXTRA_LINK_LIBS}"
 
-/* Automatic command line arguments to add when compiling with 
- * the Kitsune CUDA runtime backend target (via -ftapir=kitcuda). 
+/* Automatic command line arguments to add when compiling with
+ * the Kitsune CUDA runtime backend target (via -ftapir=kitcuda).
  */
- #cmakedefine01 KITSUNE_ENABLE_KITCUDA_TARGET 
+ #cmakedefine01 KITSUNE_ENABLE_KITCUDA_TARGET
  #define KITCUDA_LIBRARY_DIR        "${KITSUNE_LIBRARY_DIR}"
  #define KITCUDA_LINK_LIBS          "${KITCUDA_LINK_LIBS}"
  #define KITCUDA_EXTRA_LINK_LIBS    "${KITCUDA_EXTRA_LINK_LIBS}"
 
 /* Automatic command line arguments to add when compiling with
  * the OpenCL/SPIR-V runtime backend target (via -ftapir=opencl).
- */ 
+ */
 #cmakedefine01 KITSUNE_ENABLE_OPENCL_TARGET
 #define OPENCL_LIBRARY_DIR         "${OPENCL_LIBRARY_DIR}"
 #define OPENCL_LINK_LIBS           "${OPENCL_LIBRARIES}"
