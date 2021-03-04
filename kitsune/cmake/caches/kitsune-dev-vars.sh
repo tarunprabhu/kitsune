ba2027058de8f4ebbb64a19e6f60f28af6948e7b
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-03-04 12:18:03 -0700

    Updates to build configuration details (including rolling in a new module-based set of mechanisms that can be used).

--- kitsune/cmake/caches/kitsune-dev-vars.sh
+++ kitsune/cmake/caches/kitsune-dev-vars.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 #
-export KOKKOS_BASE_DIR=/projects/kitsune/x86_64
-export OPENCILKRT_BASE_DIR=/projects/kitsune/x86_64
-export QTHREADS_BASE_DIR=/projects/kitsune/x86_64
-export REALM_BASE_DIR=/projects/kitsune/x86_64
+#
+# Set the location of the various runtimes base install directory.  From here the
+# cache file will expand these variables inline to provide automatic search path
+# and required libraries (see below for details on libraries).
+#
+[ ! -z "${KOKKOS_PATH}" ] && export KOKKOS_PATH=/projects/kitsune/x86_64
+[ ! -z "${OPENCILKRTS_PATH}"] && export export OPENCILKRTS_PATH=/projects/kitsune/x86_64
+[ ! -z "${QTHREADS_PATH}"] && export QTHREADS_PATH=/projects/kitsune/x86_64
+[ ! -z "${REALM_PATH}"] && export REALM_PATH=/projects/kitsune/x86_64
+
+#
+# Depending upon how you build and configure each of these you can also provide some
+# additional enviornment variables to control what libraries are automatically included
+# in the mix when using Kitsune+Tapir.
+#
+# [ ! -z "${KOKKOS_LINK_LIBS}" ] && export KOKKOS_LINK_LIBS="-lkokkos-core -ldl -lrt"
+# [ ! -z "${OPENCILKRTS_LINK_LIBS}" ] && export OPENCILKRTS_LINK_LIBS="-lopencilkrt -l..."
+# [ ! -z "${QTHREADS_LINK_LIBS}" ] && export QTHREADS_LINK_LIBS="-lqhtread -l..."
+# [ ! -z "${REALM_LINK_LIBS}" ] && export REALM_LINK_LIBS="-lrealm -ldl -l..."
+#
+
+# Base install of all libraries with minimal additions (e.g., hwloc, etc.)
+[ ! -z "${KOKKOS_LINK_LIBS}" ] && export KOKKOS_LINK_LIBS="-lkokkos-core -ldl -lrt"
+[ ! -z "${OPENCILKRTS_LINK_LIBS}" ] && export OPENCILKRTS_LINK_LIBS="-lopencilkrt -lopencilk-personality-cpp"
+[ ! -z "${QTHREADS_LINK_LIBS}" ] && export QTHREADS_LINK_LIBS="-lqhtread"
 
 ######
 # With these enviornment variables set you can then run cmake
