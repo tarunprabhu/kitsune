66746b8cc4096bda321bdbf54a29ff258bbdc7aa
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-03-04 12:18:03 -0700

    Updates to build configuration details (including rolling in a new module-based set of mechanisms that can be used).

--- kitsune/cmake/modules/FindKokkos.cmake
+++ kitsune/cmake/modules/FindKokkos.cmake
@@ -2,7 +2,7 @@
 #
 # Kokkos_FOUND: Was a Kokkos installation found?
 # Kokkos_INCLUDE_DIR : where to find Kokkos header files.
-# Kokkos_LIBRARY_DIR : where to find the Kokkos library files. 
+# Kokkos_LIBRARY_DIR : where to find the Kokkos library files.
 # Kokkos_LINK_LIB    : the libraries to link against to use Kokkos.
 
 message(STATUS "Looking for kokkos...")
@@ -10,28 +10,31 @@ message(STATUS "Looking for kokkos...")
 find_path(Kokkos_INCLUDE_DIR Kokkos_Core.hpp
   PATHS /usr/local/include
         /opt/include
-	/opt/local/include 
+	      /opt/local/include
+        $ENV{KOKKOS_PATH}/include
 )
 
 find_library(Kokkos_LIBRARY kokkoscore
   PATHS /usr/local/lib64
         /usr/local/lib
         /opt/lib64
-        /opt/lib	
-	/opt/local/lib64
-	/opt/local/lib
+        /opt/lib
+	      /opt/local/lib64
+	      /opt/local/lib
+        $ENV{KOKKOS_PATH}/lib
+        $ENV{KOKKOS_PATH}/lib64
 )
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(Kokkos DEFAULT_MSG
-				  Kokkos_INCLUDE_DIR 
+				  Kokkos_INCLUDE_DIR
 				  Kokkos_LIBRARY)
 
 if (Kokkos_FOUND)
 
   message(STATUS "Kokkos found.")
-  
+
   get_filename_component(Kokkos_LIBRARY_DIR
                          ${Kokkos_LIBRARY}
                          DIRECTORY
@@ -42,14 +45,13 @@ if (Kokkos_FOUND)
   set(Kokkos_LINK_LIBS "-lkokkoscore"
     CACHE
     STRING "List of libraries needed for Kokkos linking.")
-  
+
   message(STATUS " Kokkos include directories: ${Kokkos_INCLUDE_DIR}")
   message(STATUS " Kokkos library directories: ${Kokkos_LIBRARY_DIR}")
   message(STATUS " Kokkos link libraries     : ${Kokkos_LINK_LIBS}")
-  
+
 else()
 
   message(STATUS "Could not find Kokkos.")
-  
-endif()
 
+endif()
