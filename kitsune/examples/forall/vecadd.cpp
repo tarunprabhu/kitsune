ee68c6ce558ed516f11cac96b1308a438aa556c8
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-03-09 12:24:41 -0700

    Some more build and compilation configuration tweaks (bug fixes) and the start of updated documentation for the milestone/release.
    A big of debugging/tweaking to the realm runtime for hunting bugs.  At this point
    it appears to work correctly w/out optimization enabled (i.e., use -O0).  With any
    level of optimization enabled the code currently fails some simple regressions but
    details are currently unclear as to why (e.g., race, bad code, etc.).

--- kitsune/examples/forall/vecadd.cpp
+++ kitsune/examples/forall/vecadd.cpp
@@ -1,6 +1,6 @@
-// Very simple test of kokkos with two common forms of the 
-// parallel_for construct.  We should be able to transform 
-// all constructs from lambda into simple loops... 
+// Very simple test of kokkos with two common forms of the
+// parallel_for construct.  We should be able to transform
+// all constructs from lambda into simple loops...
 #include <cstdio>
 #include <cstddef>
 #include <cstdlib>
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-const size_t VEC_SIZE = 1024 * 1024 * 256;
+const size_t VEC_SIZE = 4096; //  * 4096;
 
 int main (int argc, char* argv[]) {
 
@@ -20,25 +20,26 @@ int main (int argc, char* argv[]) {
   for(size_t i = 0; i < VEC_SIZE; ++i) {
       A[i] = rand() / (float)RAND_MAX;
       B[i] = rand() / (float)RAND_MAX;
-  } 
+  }
 
-  forall(size_t i = 0; i < VEC_SIZE; ++i) { 
+  forall(size_t i = 0; i < VEC_SIZE; ++i) {
     C[i] = A[i] + B[i];
   }
 
   size_t ti = 0;
   for(; ti < VEC_SIZE; ++ti) {
     float sum = A[ti] + B[ti];
-    if (fabs(C[ti] - sum) > 1e-7f)
+    if (fabs(C[ti] - sum) > 1e-7f) {
+      fprintf(stdout, "incorrect result -- delta = %f\n", C[ti] - sum);
       break; // whoops...
+    }
   }
- 
+
   fprintf(stdout, "Result = %s\n", (ti == VEC_SIZE) ? "PASS" : "FAIL");
 
   delete []A;
   delete []B;
   delete []C;
-  
+
   return 0;
 }
-
