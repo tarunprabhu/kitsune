473c05b2b7387d51fc462abdfb7bbf0995714847
Patrick Flynn <patrickf4664@protonmail.com>
2021-05-27 15:33:58 -0400

    Added support for computing upper bound of 2-D array

--- clang/lib/CodeGen/CGKokkos.cpp
+++ clang/lib/CodeGen/CGKokkos.cpp
@@ -219,7 +219,18 @@ void CodeGenFunction::EmitKokkosParallelForCond(const Expr *BoundsExpr,
   if (BoundsExpr->getStmtClass() == Expr::BinaryOperatorClass) {
     RValue RV = EmitAnyExpr(BoundsExpr);
     LoopEnd = RV.getScalarVal();
-  } else {
+  } else if (BoundsExpr->getStmtClass() == Expr::CXXTemporaryObjectExprClass) {
+    const CXXTemporaryObjectExpr *CXXTO = dyn_cast<CXXTemporaryObjectExpr>(BoundsExpr);
+    const InitListExpr *UpperBounds = dyn_cast<InitListExpr>(CXXTO->getArg(1)->IgnoreImplicit());
+    
+    // Create a multiply statement to computer the proper upper bound
+    const Expr *lval = UpperBounds->getInit(0)->IgnoreImplicit();
+    const Expr *rval = UpperBounds->getInit(1)->IgnoreImplicit();
+    
+    llvm::Value *lvalue = EmitScalarExpr(lval);
+    llvm::Value *rvalue = EmitScalarExpr(rval);
+    LoopEnd = Builder.CreateMul(lvalue, rvalue);
+  } else { 
     LoopEnd = EmitScalarExpr(BoundsExpr);
   }
 
@@ -267,17 +278,19 @@ bool CodeGenFunction::EmitKokkosParallelFor(const CallExpr *CE,
     return false;
   }
   
-  // Check to see if we have an MDRange present
-  if (BE->getStmtClass() == Expr::CXXTemporaryObjectExprClass) {
+  // Handle a potential multi-dimensional array
+  /*if (BE->getStmtClass() == Expr::CXXTemporaryObjectExprClass) {
     const CXXTemporaryObjectExpr *CXXTO = dyn_cast<CXXTemporaryObjectExpr>(BE);
-    std::string className = CXXTO->getBestDynamicClassType()->getNameAsString();
+    const InitListExpr *UpperBounds = dyn_cast<InitListExpr>(CXXTO->getArg(1)->IgnoreImplicit());
     
-    if (className == "MDRangePolicy") {
-      return EmitKokkosParallelForMD(CE, PFName, BE, Lambda, ForallAttrs);
-    } else {
-      // What should we do here?
-    }
-  }
+    // Create a multiply statement to computer the proper upper bound
+    const Expr *lval = UpperBounds->getInit(0)->IgnoreImplicit();
+    const Expr *rval = UpperBounds->getInit(1)->IgnoreImplicit();
+    
+    llvm::Value *lvalue = EmitScalarExpr(lval);
+    llvm::Value *rvalue = EmitScalarExpr(rval);
+    BE = Builder.CreateMul(lvalue, rvalue);
+  }*/
 
   // Create all jump destinations and basic blocks in the order they
   // appear in the IR.
