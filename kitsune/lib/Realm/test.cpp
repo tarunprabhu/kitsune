5fff0d60809450ccf7c37a89657a8dc3009ce4ef
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-06-29 19:21:29 -0600

    Work in progress -- trying to fix some build and broken configuration settings and seeing some odd regressions in performance that don't make a lot of sense to me yet.  Pushing this up to capture the current (broken) state for other to have a look-see...

--- kitsune/lib/Realm/test.cpp
+++ kitsune/lib/Realm/test.cpp
@@ -5,16 +5,16 @@
 
 
 extern "C" {
-Realm::Barrier createRealmBarrier(); 
-void destroyRealmBarrier(Realm::Barrier& b); 
-void realmInitRuntime(int argc, char**argv); 
+Realm::Barrier createRealmBarrier();
+void destroyRealmBarrier(Realm::Barrier& b);
+void realmInitRuntime(int argc, char**argv);
 void realmSpawn(Realm::Barrier& b,
-      Realm::Processor::TaskFuncPtr func, 
-		  const void* args, 
+      Realm::Processor::TaskFuncPtr func,
+		  const void* args,
 		  size_t arglen
-      ); 
-void realmSync(Realm::Barrier& b); 
-size_t realmGetNumProcs(); 
+      );
+void realmSync(Realm::Barrier& b);
+size_t realmGetNumProcs();
   typedef struct context {
     Realm::Runtime rt;
     std::set<Realm::Event> events;
@@ -23,24 +23,24 @@ size_t realmGetNumProcs();
     size_t numprocs;
     unsigned cur_task;
   } context;
-context getRealmCTX(); 
+context getRealmCTX();
 }
 
 void task(const void* args, uint64_t arglen, const void* mem, uint64_t memlen, Realm::Processor p){
   int i = *(int*)args;
   std::this_thread::sleep_for(std::chrono::seconds(1));
-  printf("hello from task %d / %d\n", i, realmGetNumProcs()); 
+  printf("hello from task %d / %d\n", i, realmGetNumProcs());
 }
 
 
 int main(int argc, char** argv){
-  realmInitRuntime(argc, argv); 
+  realmInitRuntime(argc, argv);
   Realm::Barrier b = createRealmBarrier();
-  auto n = realmGetNumProcs(); 
-  int a[n]; for(int i=0; i < n; i++) a[i] = i;  
-  for(int i=0; i<n; i++){
+  auto n = realmGetNumProcs();
+  int a[n]; for(unsigned int i=0; i < n; i++) a[i] = i;
+  for(unsigned int i=0; i<n; i++){
     realmSpawn(b, &task, (void*)(a+i), sizeof(int));
   }
-  realmSync(b); 
-  destroyRealmBarrier(b); 
+  realmSync(b);
+  destroyRealmBarrier(b);
 }
