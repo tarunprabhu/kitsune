7d4913ecebdeeb954e21252b249a1b495d684b0d
TB Schardl <neboat@mit.edu>
2021-07-22 22:58:25 +0000

    [CodeGen][Sema] Cleanup Cilk-related code, primarily around code-generation for implicit syncs.

--- clang/lib/CodeGen/CGStmt.cpp
+++ clang/lib/CodeGen/CGStmt.cpp
@@ -1320,9 +1320,6 @@ void CodeGenFunction::EmitReturnStmt(const ReturnStmt &S) {
     Builder.ClearInsertionPoint();
   }
 
-  if (getLangOpts().getCilk() != LangOptions::Cilk_none)
-    EmitImplicitSyncCleanup();
-
   // Emit the result value, even if unused, to evaluate the side effects.
   const Expr *RV = S.getRetValue();
 
@@ -1415,7 +1412,9 @@ void CodeGenFunction::EmitReturnStmt(const ReturnStmt &S) {
     IsSpawned = false;
     PopDetachScope();
   }
-  EmitBranchThroughCleanup(ReturnBlock);
+
+  bool CompilingCilk = (getLangOpts().getCilk() != LangOptions::Cilk_none);
+  EmitBranchThroughCleanup(ReturnBlock, CompilingCilk);
 }
 
 void CodeGenFunction::EmitDeclStmt(const DeclStmt &S) {
