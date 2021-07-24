100c53c2c9cf10d6a54fd2ff818ab44abc758619
TB Schardl <neboat@mit.edu>
2021-07-24 16:15:06 +0000

    [clang/Cilk][Intrinsics] Add _Cilk_scope construct, a lexical scope that guarantees upon exit to _Cilk_sync any tasks spawned within the scope.  Add intrinsics to allow the _Cilk_scope construct to mark where a Tapir-target runtime may be started and stopped at the beginning and end of the scope, respectively.
    These intrinsics are meant only to be hints to a Tapir-target runtime,
    not definitive markers for where the runtime must be started or
    stopped.  This requirement is necessary to make it safe to nest
    _Cilk_scopes, either within the same function or indirectly
    via function calls.
    
    No particular compiler optimizations are included for these
    intrinsics, although some optimizations may be useful to add in the
    future.

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -5180,6 +5180,7 @@ public:
                                   bool SupressSimplerImplicitMoves = false);
 
   void DiagnoseCilkSpawn(Stmt *S);
+  StmtResult ActOnCilkScopeStmt(SourceLocation ScopeLoc, Stmt *S);
   StmtResult ActOnCilkSyncStmt(SourceLocation SyncLoc);
   StmtResult ActOnCilkSpawnStmt(SourceLocation SpawnLoc, Stmt *S);
   ExprResult ActOnCilkSpawnExpr(SourceLocation SpawnLoc, Expr *E);
