e28d6dd99cdaa7c229c84dd8a0dbab37e24d449f
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-08-16 12:30:52 -0600

    More work to sort through adding the Realm ABI target into the build system w/ realm as a FetchContent component.  This means some dependency and target work within cmake, some ordering details about where the FetchContent occurs, and also some fixes to the code base that were lost for realm support (e.g., missing switch entries).
    There is some point where the overall cmake config becomes more difficult
    to reason about than the llvm+clang+... source code.  ;-)
    
    A few additional fixes for the config file (.cfg) settings and supporting
    cmake pieces.  The default config files are autogenerated to handle both
    the in-tree and install use cases.  It is probably worth tailoring for
    your own use cases at some point but adding them to your user directory.
    
    TODO: there is currently a "collision" between the use of a cmake option
    and the ABI libraries to build within kitsune.  At present you will have
    both enable and add the abi library to the configuration (see the cache
    file under kitsune/cmake/caches/kitsune-dev.cmake).

---
+++ kitsune/cmake/modules/KitsuneUtils.cmake
@@ -0,0 +1,61 @@
+#
+# Get a list of all enabled tapir runtime targets so we can
+# walk through each and do "stuff" (e.g., build an executable
+# for each enabled target from a single source file).
+#
+# NOTE: This implementation assumes there is a one-to-one
+# match between the compiler option flag and the name used
+# in the CMake variables.  In other words,
+#
+#   -ftapir=TARGET_NAME
+#
+# has a corresponding CMAKE configuration flag:
+#
+#   KITSUNE_ENABLE_(TARGET_NAME)_TARGET
+#
+# In addition, the list '_rt_cmake_var_names' below will need
+# to be updated to include any new runtime targets added to
+# Kitsune+Tapir.
+#
+macro(get_tapir_rt_targets rt_list)
+  set(_rt_cmake_var_names "OPENMP;QTHREADS;REALM;CUDATK;HIP;OPENCL")
+  # Always enable the serial and opencilk targets.
+  list(APPEND ${rt_list} "serial")
+  list(APPEND ${rt_list} "opencilk")
+
+  foreach(rt IN ITEMS ${_rt_cmake_var_names})
+    set(_enabled_var "KITSUNE_ENABLE_${rt}_TARGET")
+    message(STATUS "checking for ${_enabled_var}")
+    if (${_enabled_var})
+      string(TOLOWER ${rt} flag)
+      list(APPEND ${rt_list} ${flag})
+    endif()
+  endforeach()
+  unset(_rt_cmake_var_names)
+  unset(_kitsune_rt_flags)
+endmacro()
+
+# NOTE: the dep_list and target_libs variables used in the following 
+# function are expected to live in the parent scope (i.e., we're 
+# mucking with state outside of the function to try and debug some 
+# build dependency issues).  
+function(add_tapir_dependency target abi)
+  message(STATUS "adding dependency for ${target} w/ -ftapir=${abi}")
+  if (${abi} STREQUAL "opencilk")
+    set(dep_list ${dep_list} cheetah PARENT_SCOPE)
+    set(target_libs ${target_libs} opencilk opencilk-personality-cpp PARENT_SCOPE)
+  elseif(${abi} STREQUAL "realm")
+    set(dep_list ${dep_list} RealmABI PARENT_SCOPE)
+    set(target_libs ${target_libs} realm-abi realm PARENT_SCOPE)
+  elseif(${abi} STREQUAL "none")
+    message(STATUS "no dependencies for '-ftapir=none'...")
+  elseif(${abi} STREQUAL "serial")
+    message(STATUS "no dependencies for '-ftapir=serial'...")
+  else()
+    message(FATAL_ERROR
+       "tapir dependency ${abi} not handled in add_tapir_dependency")
+  endif()
+  if (dep_list) 
+    add_dependencies(${target} ${dep_list})
+  endif()
+endfunction()
