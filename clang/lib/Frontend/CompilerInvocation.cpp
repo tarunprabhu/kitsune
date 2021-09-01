19227307eebdb27537e4b5fb95ad59ca906f67e3
George Stelle <stelleg@gmail.com>
2021-09-01 11:12:53 -0600

    Handling kernel arguments

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -4715,6 +4715,8 @@ bool CompilerInvocation::CreateFromArgsImpl(
       LangOpts.Tapir = TapirTargetID::Realm;
     else if (Name == "opencl")
       LangOpts.Tapir = TapirTargetID::OpenCL;
+    else if (Name == "gpu")
+      LangOpts.Tapir = TapirTargetID::GPU;
     else if (Name == "serial")
       LangOpts.Tapir = TapirTargetID::Serial;
     else
