af6993d925aa9cda392315f636bd0de46069f29f
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-09-01 16:12:31 -0600

    Some clean up to remove the cudakit target (towards unification across the toolchain between tapir, kitsune, opencilk, etc.).
    New cuba abi runtime target code to simplify code gen -- needs to
    merge design ideas with George's current multi-architecture approach.

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -2534,14 +2534,14 @@ void CompilerInvocation::ParseLangArgs(LangOptions &Opts, ArgList &Args,
   // Check if -fopenmp is specified and set default version to 4.5.
   Opts.OpenMP = Args.hasArg(options::OPT_fopenmp) ? 45 : 0;
 
-  // Check if -fkitsune is specified. 
+  // Check if -fkitsune is specified.
   Opts.Kitsune = Args.hasArg(options::OPT_fkitsune) ? 1 : 0;
-  
-  // Check if -fkokkos is specified. 
+
+  // Check if -fkokkos is specified.
   Opts.Kokkos = Args.hasArg(options::OPT_fkokkos) ? 1 : 0;
   Opts.KokkosNoInit = Args.hasArg(options::OPT_fkokkos_no_init) ? 1: 0;
 
-  // Check if -fflecsi is specified. 
+  // Check if -fflecsi is specified.
   Opts.FleCSI = Args.hasArg(options::OPT_fflecsi) ? 1 : 0;
 
   // Check if -fopenmp-simd is specified.
@@ -3033,10 +3033,10 @@ bool CompilerInvocation::CreateFromArgs(CompilerInvocation &Res,
     StringRef Name = A->getValue();
     if (Name == "none")
       LangOpts.Tapir = TapirTargetID::None;
-    else if (Name == "cilk") 
+    else if (Name == "cilk")
       LangOpts.Tapir = TapirTargetID::Cilk;
     else if (Name == "opencilk")
-      LangOpts.Tapir = TapirTargetID::OpenCilk; 
+      LangOpts.Tapir = TapirTargetID::OpenCilk;
     else if (Name == "openmp")
       LangOpts.Tapir = TapirTargetID::OpenMP;
     else if (Name == "qthreads")
@@ -3045,12 +3045,10 @@ bool CompilerInvocation::CreateFromArgs(CompilerInvocation &Res,
       LangOpts.Tapir = TapirTargetID::Realm;
     else if (Name == "cuda")
       LangOpts.Tapir = TapirTargetID::Cuda;
-    else if (Name == "kitcuda")
-      LangOpts.Tapir = TapirTargetID::KitCuda;
     else if (Name == "realm")
       LangOpts.Tapir = TapirTargetID::Realm;
     else if (Name == "opencl")
-      LangOpts.Tapir = TapirTargetID::OpenCL;    
+      LangOpts.Tapir = TapirTargetID::OpenCL;
     else if (Name == "serial")
       LangOpts.Tapir = TapirTargetID::Serial;
     else
