26b76633498f81f45024e495eb77a7b1155e0c5b
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-09-08 14:02:44 -0600

    prep to merge w/ an update from George.

--- kitsune/runtimes/gpu/check-hip.cc
+++
@@ -1,6 +0,0 @@
-#if __has_include("hip/hip_runtime.h")
-#include"llvm-hip.cc"
-#else
-#warning "Couldn't find hip, not building hip support"
-#include"nohip.cc"
-#endif
--- kitsune/runtimes/gpu/llvm-spirv.h
+++
@@ -1,7 +0,0 @@
-#include<stdbool.h>
-#include<dlfcn.h>
-#include<llvm/IR/Module.h>
-
-bool initSPIRV();
-void* launchSPIRVKernel(llvm::Module& kernel, void**args, size_t n);
-void waitSPIRVKernel(void* wait);
