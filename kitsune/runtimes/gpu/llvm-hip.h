8f9efc9a12ed56602e8258e7cc28a11921edd6f7
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-09-08 14:02:44 -0600

    prep to merge w/ an update from George.

--- kitsune/runtimes/gpu/llvm-hip.h
+++
@@ -1,6 +0,0 @@
-#include<llvm/Target/TargetMachine.h>
-
-void* hipManagedMalloc(size_t n);
-int initHIP();
-void* launchHIPKernel(llvm::Module& m, void** args, size_t n); 
-void waitHIPKernel(void* wait); 
