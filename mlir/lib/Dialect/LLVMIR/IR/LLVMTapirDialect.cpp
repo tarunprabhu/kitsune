165b248c8e04c97d23cf672f4a7da0163e3d2604
JacobMarksLANL <jmarks@lanl.gov>
2022-01-25 12:57:06 -0700

    Added the basic start of the LLVMTapir Dialect. Currently does not compile correctly.

---
+++ mlir/lib/Dialect/LLVMIR/IR/LLVMTapirDialect.cpp
@@ -0,0 +1,31 @@
+//===- LLVMTapirDialect.cpp - MLIR LLVMSVE ops implementation ------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the LLVMTapir dialect and its operations.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/IR/IntrinsicsAArch64.h" //Presumably this needs changed
+
+#include "mlir/Dialect/LLVMIR/LLVMTapirDialect.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/TypeUtilities.h"
+
+using namespace mlir;
+
+void LLVM::LLVMTapirDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "mlir/Dialect/LLVMIR/LLVMTapir.cpp.inc"
+      >();
+}
+
+#define GET_OP_CLASSES
+#include "mlir/Dialect/LLVMIR/LLVMTapir.cpp.inc"
