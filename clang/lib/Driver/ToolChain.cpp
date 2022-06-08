dde26a27e37c93b2b0090133e5331f2136d037fe
George Stelle <stelleg@gmail.com>
2022-06-08 12:29:16 -0600

    Fixed libopencilk bitcode clang driver check

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -39,6 +39,7 @@
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/FileUtilities.h"
 #include "llvm/Support/Path.h"
+#include "llvm/Support/Process.h"
 #include "llvm/Support/TargetParser.h"
 #include "llvm/Support/VersionTuple.h"
 #include "llvm/Support/VirtualFileSystem.h"
@@ -1520,6 +1521,7 @@ void ToolChain::AddOpenCilkABIBitcode(const ArgList &Args,
     if (IsLTO)
       CmdArgs.push_back(
           Args.MakeArgString("--plugin-opt=opencilk-abi-bitcode=" + P));
+    return; 
   }
 
   bool UseAsan = getSanitizerArgs(Args).needsAsanRt();
@@ -1533,6 +1535,12 @@ void ToolChain::AddOpenCilkABIBitcode(const ArgList &Args,
                                            *OpenCilkABIBCFilename));
     return;
   }
+
+  // Check if libopencilk is in LD_LIBRARY_PATH, and if it is, we're OK
+  if(llvm::sys::Process::FindInEnvPath("LD_LIBRARY_PATH", "libopencilk-abi.bc").hasValue()){
+    return;
+  }
+
 
   // Error if we could not find a bitcode file.
   getDriver().Diag(diag::err_drv_opencilk_missing_abi_bitcode)
