bc3acc8464e750cf9ce7b9fa89e60f19e28ffc9f
Tarun Prabhu <tarun.prabhu@gmail.com>
2022-06-21 09:46:05 -0500

    Merge upstream

--- kitsune/experiments/saxpy/saxpy-forall.cpp
+++ kitsune/experiments/saxpy/saxpy-forall.cpp
@@ -34,38 +34,30 @@ bool check_saxpy(const float *v, size_t N) {
   fprintf(stderr, "Error: %f\n", err);
   return err == 0.0f;
 }
- 
+
 int main(int argc, char *argv[]) {
 
   size_t N = DEFAULT_SIZE;
   if (argc > 1) 
     N = atol(argv[1]);
 
-  printf("array size: %zu\n", N);
-  printf("X value: %f\n", DEFAULT_X_VALUE);
-  printf("Y value: %f\n", DEFAULT_Y_VALUE);
-  printf("A value: %f\n", DEFAULT_A_VALUE);
-
   float *x = (float*)__kitrt_cuMemAllocManaged(sizeof(float) * N);
   float *y = (float*)__kitrt_cuMemAllocManaged(sizeof(float) * N);
+
+  __kitrt_cuEnableEventTiming();  
 
   forall(size_t i = 0; i < N; i++) {
     x[i] = DEFAULT_X_VALUE;
     y[i] = DEFAULT_Y_VALUE;
   }
-  __kitrt_cuDisableEventTiming();
 
-  printf("x[0] = %f\n", x[0]);
-
-  __kitrt_cuEnableEventTiming();
   forall(size_t i = 0; i < N; i++) {
     y[i] = DEFAULT_A_VALUE * x[i] + y[i];
   }
-  __kitrt_cuDisableEventTiming();
 
   if (! check_saxpy(y, N)) 
     return 1;
-  else 
+  else
     return 0;
 }
 
