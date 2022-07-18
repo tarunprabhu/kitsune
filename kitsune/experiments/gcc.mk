67d46251ee6028a39f14c180a3fd65f99a815418
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2022-07-18 16:43:46 -0600

    Some fixes, simplification of srad benchmark for measuring overall runtime (in prep for graph rt code gen and support).  New Rodina benchmark (euler3d) in forall form.

--- kitsune/experiments/gcc.mk
+++ kitsune/experiments/gcc.mk
@@ -1,4 +1,5 @@
-gcc_prefix=/projects/gcc
-gcc=${gcc_prefix}/bin/gcc
-gccxx=${gcc_prefix}/bin/g++
+gcc_prefix=/usr/
+#gcc_prefix=/projects/gcc
+gcc=${gcc_prefix}/bin/gcc-10
+gccxx=${gcc_prefix}/bin/g++-10
 
