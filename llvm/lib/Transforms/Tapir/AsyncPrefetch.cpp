deaf8c1f0921568a74b86d6728d42a1e914c5806
Tarun Prabhu <tarun.prabhu@gmail.com>
2022-07-30 11:12:35 -0500

    WIP: No need to collect loops recusively

--- llvm/lib/Transforms/Tapir/AsyncPrefetch.cpp
+++ llvm/lib/Transforms/Tapir/AsyncPrefetch.cpp
@@ -25,6 +25,10 @@
 
 using namespace llvm;
 
+// forall loops L1, L2, ... Ln are deemed to be siblings if they share the
+// same parent loop Lp or if none of L1, L2 ... Ln have a parent loop i.e. if
+// all Li are top-level loops within the function. There could be an arbitrary
+// amount of code between each Li even if they are siblings.
 using SiblingForalls = std::vector<const Loop *>;
 
 class AsyncPrefetchImpl {
@@ -34,8 +38,7 @@ private:
   AliasAnalysis &AA;
 
 private:
-  void getLoops(const Loop *loop, std::set<const Loop *> &loops);
-  std::vector<const Loop *> getLoops();
+  bool isAdjacent(const Loop* li, const Loop *lj);
   bool isForall(const Loop *loop);
   std::vector<SiblingForalls> getSiblingForalls();
 
@@ -48,30 +51,13 @@ public:
   void runOnFunction(Function &F);
 };
 
-void AsyncPrefetchImpl::getLoops(const Loop *loop,
-                                 std::set<const Loop *> &loops) {
-  if (loops.find(loop) != loops.end())
-    return;
-
-  loops.insert(loop);
-  for (const Loop *subLoop : *loop)
-    getLoops(subLoop, loops);
-}
-
-std::vector<const Loop *> AsyncPrefetchImpl::getLoops() {
-  std::set<const Loop *> loops;
-  for (const Loop *loop : LI)
-    getLoops(loop, loops);
-  return std::vector<const Loop *>(loops.begin(), loops.end());
-}
-
 bool AsyncPrefetchImpl::isForall(const Loop *loop) {
   return getTaskIfTapirLoop(loop, &TI);
 }
 
 std::vector<SiblingForalls> AsyncPrefetchImpl::getSiblingForalls() {
   std::map<const Loop *, SiblingForalls> m;
-  for (const Loop *loop : getLoops()) {
+  for (const Loop *loop : LI.getLoopsInPreorder()) {
     if (isForall(loop))
       m[loop->getParentLoop()].push_back(loop);
   }
@@ -82,21 +68,62 @@ std::vector<SiblingForalls> AsyncPrefetchImpl::getSiblingForalls() {
   return ret;
 }
 
+bool AsyncPrefetchImpl::isAdjacent(const Loop* li, const Loop* lj) {
+  // An exit block is a block outside the loop to which the loop body
+  // branches when the loop terminates. We require the loop to have a single
+  // exit block;
+  BasicBlock* exit = li->getExitBlock();
+
+  // The preheader is the unique block which branches to the loop header.
+  BasicBlock* preheader = lj->getLoopPreheader();
+
+  if (exit and preheader) {
+    BasicBlock* succ = exit->getSingleSuccessor();
+    BasicBlock* pred = preheader->getSinglePredecessor();
+    if (succ and pred) {
+      if (succ == pred) {
+        llvm::errs() << li->getName() << " => " << lj->getName() << "\n";
+        llvm::errs() << "exit\n" << *exit << "\n";
+        llvm::errs() << "common\n" << *succ << "\n";
+        llvm::errs() << "preheader\n" << *preheader << "\n";
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
 void AsyncPrefetchImpl::runOnFunction(Function &F) {
+  llvm::errs() << "loops\n";
+  for (const Loop* loop : LI.getLoopsInPreorder())
+    llvm::errs() << "  " << loop << " [" << loop->getName() << "]\n";
+
   for (const SiblingForalls &siblings : getSiblingForalls()) {
     llvm::errs() << "begin sibling group\n";
-    std::set<Value *> all;
+    for (const Loop* loop : siblings)
+      llvm::errs() << "  " << loop->getName() << "\n";
+    std::vector<const Loop*> adj;
+    for (size_t i = 0; i < siblings.size() - 1; i++) {
+      const Loop* li = siblings[i];
+      const Loop* lj = siblings[i + 1];
+      isAdjacent(li, lj);
+    }
     for (const Loop *loop : siblings) {
       BasicBlock *header = loop->getLoopPreheader() ? loop->getLoopPreheader()
                                                     : loop->getHeader();
       BasicBlock *exit = loop->getExitBlock();
 
+      // Two loops Li and Lj are adjacent if they are siblings and there is no
+      // executable code except an unconditional branch between the exit block
+      // of Li and the preheader of Lj.
       for (BasicBlock *pred : predecessors(header))
-        llvm::errs() << "pred: " << pred << " => [" << pred->getName() << "]\n";
-      llvm::errs() << "hder: " << header << " => [" << header->getName() << "]\n";
+        llvm::errs() << "pred: " << pred << " => [" << pred->getName() << "] " << LI[pred] << "\n";
+      llvm::errs() << "hder: " << header << " => [" << header->getName()
+                   << "]\n";
       llvm::errs() << "exit: " << exit << " => [" << exit->getName() << "]\n";
       for (BasicBlock *succ : successors(exit))
-        llvm::errs() << "succ: " << succ << " => [" << succ->getName() << "]\n";
+        llvm::errs() << "succ: " << succ << " => [" << succ->getName() << "] " << LI[succ] << "\n";
       llvm::errs() << "---------------\n";
     }
     llvm::errs() << "end sibling group\n\n";
