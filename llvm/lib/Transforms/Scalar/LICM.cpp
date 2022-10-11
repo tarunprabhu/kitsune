OpenCilk/opencilk-project

[LICM] Enable loop-invariant-code motion of strand_pure function calls out of serial loops

llvm/lib/Transforms/Scalar/LICM.cpp
@@ -1150,7 +1150,8 @@ bool llvm::canSinkOrHoistInst(Instruction &I, AAResults *AA, DominatorTree *DT,
       // A readonly argmemonly function only reads from memory pointed to by
       // it's arguments with arbitrary offsets.  If we can prove there are no
       // writes to this memory in the loop, we can hoist or sink.
-      if (AliasAnalysis::onlyAccessesArgPointees(Behavior)) {
+      if (AliasAnalysis::onlyAccessesArgPointees(Behavior) ||
+          CI->isStrandPure()) {
         // TODO: expand to writeable arguments
         for (Value *Op : CI->arg_operands())
           if (Op->getType()->isPointerTy()) {
@@ -1704,6 +1705,14 @@ static bool isSafeToExecuteUnconditionally(Instruction &Inst,
                                            const Instruction *CtxI) {
   if (isSafeToSpeculativelyExecute(&Inst, CtxI, DT))
     return true;
+
+  if (CtxI)
+    if (const CallBase *CB = dyn_cast<CallBase>(&Inst)) {
+      const Function *Callee = CB->getCalledFunction();
+      if (Callee && Callee->isStrandPure())
+        return (TI->getSpindleFor(Inst.getParent()) ==
+                TI->getSpindleFor(CtxI->getParent()));
+    }
 
   bool GuaranteedToExecute =
       SafetyInfo->isGuaranteedToExecute(Inst, DT, TI, CurLoop);

tarunprabhu/kitsune

llvm/lib/Transforms/Scalar/LICM.cpp
@@ -1214,7 +1214,8 @@ bool llvm::canSinkOrHoistInst(Instruction &I, AAResults *AA, DominatorTree *DT,
       // A readonly argmemonly function only reads from memory pointed to by
       // it's arguments with arbitrary offsets.  If we can prove there are no
       // writes to this memory in the loop, we can hoist or sink.
-      if (AAResults::onlyAccessesArgPointees(Behavior)) {
+      if (AAResults::onlyAccessesArgPointees(Behavior) ||
+          CI->isStrandPure()) {
         // TODO: expand to writeable arguments
         for (Value *Op : CI->args())
           if (Op->getType()->isPointerTy() &&
@@ -1749,6 +1750,14 @@ static bool isSafeToExecuteUnconditionally(
     bool AllowSpeculation) {
   if (AllowSpeculation && isSafeToSpeculativelyExecute(&Inst, CtxI, DT, TLI))
     return true;
+
+  if (CtxI)
+    if (const CallBase *CB = dyn_cast<CallBase>(&Inst)) {
+      const Function *Callee = CB->getCalledFunction();
+      if (Callee && Callee->isStrandPure())
+        return (TI->getSpindleFor(Inst.getParent()) ==
+                TI->getSpindleFor(CtxI->getParent()));
+    }
 
   bool GuaranteedToExecute =
       SafetyInfo->isGuaranteedToExecute(Inst, DT, TI, CurLoop);