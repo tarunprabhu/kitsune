2b206d7c526953f40f230afc654c6642cf474483
Tobias Hieta <tobias@hieta.se>
2023-08-28 12:13:13 +0200

    Revert "[clang] Set FP options in Sema when instantiating CompoundStmt"
    This reverts commit f1d5ea362577a8a1b5fafe775cf82a449daa3b07.

--- clang/lib/Sema/TreeTransform.h
+++ clang/lib/Sema/TreeTransform.h
@@ -7531,10 +7531,6 @@ StmtResult
 TreeTransform<Derived>::TransformCompoundStmt(CompoundStmt *S,
                                               bool IsStmtExpr) {
   Sema::CompoundScopeRAII CompoundScope(getSema());
-  Sema::FPFeaturesStateRAII FPSave(getSema());
-  if (S->hasStoredFPFeatures())
-    getSema().resetFPOptions(
-        S->getStoredFPFeatures().applyOverrides(getSema().getLangOpts()));
 
   const Stmt *ExprResult = S->getStmtExprResult();
   bool SubStmtInvalid = false;
