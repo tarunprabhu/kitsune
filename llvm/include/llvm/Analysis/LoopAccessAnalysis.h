OpenCilk/opencilk-project

[LoopAccessAnalysis] Add support for data-race-free dependence analysis.

llvm/include/llvm/Analysis/LoopAccessAnalysis.h
@@ -162,7 +162,8 @@ public:
                const SmallVectorImpl<Instruction *> &Instrs) const;
   };
 
-  MemoryDepChecker(PredicatedScalarEvolution &PSE, const Loop *L)
+  MemoryDepChecker(PredicatedScalarEvolution &PSE, const Loop *L,
+                   TaskInfo *TI = nullptr)
       : PSE(PSE), InnermostLoop(L), AccessIdx(0), MaxSafeRegisterWidth(-1U),
         ShouldRetryWithRuntimeCheck(false), SafeForVectorization(true),
         RecordDependences(true) {}
@@ -281,6 +282,9 @@ private:
   /// Memory dependences collected during the analysis.  Only valid if
   /// RecordDependences is true.
   SmallVector<Dependence, 8> Dependences;
+
+  /// Optional TaskInfo
+  TaskInfo *TI;
 
   /// Check whether there is a plausible dependence between the two
   /// accesses.
@@ -493,7 +497,8 @@ private:
 class LoopAccessInfo {
 public:
   LoopAccessInfo(Loop *L, ScalarEvolution *SE, const TargetLibraryInfo *TLI,
-                 AliasAnalysis *AA, DominatorTree *DT, LoopInfo *LI);
+                 AliasAnalysis *AA, DominatorTree *DT, LoopInfo *LI,
+                 TaskInfo *TI = nullptr);
 
   /// Return true we can analyze the memory accesses in the loop and there are
   /// no memory dependence cycles.
@@ -581,7 +586,8 @@ public:
 private:
   /// Analyze the loop.
   void analyzeLoop(AliasAnalysis *AA, LoopInfo *LI,
-                   const TargetLibraryInfo *TLI, DominatorTree *DT);
+                   const TargetLibraryInfo *TLI, DominatorTree *DT,
+                   TaskInfo *TI);
 
   /// Check if the structure of the loop allows it to be analyzed by this
   /// pass.
@@ -728,6 +734,7 @@ private:
   AliasAnalysis *AA;
   DominatorTree *DT;
   LoopInfo *LI;
+  TaskInfo *TI = nullptr;
 };
 
 /// This analysis provides dependence information for the memory

llvm/lib/Analysis/LoopAccessAnalysis.cpp
@@ -33,6 +33,7 @@
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Analysis/ScalarEvolutionExpander.h"
 #include "llvm/Analysis/ScalarEvolutionExpressions.h"
+#include "llvm/Analysis/TapirTaskInfo.h"
 #include "llvm/Analysis/TargetLibraryInfo.h"
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/Analysis/VectorUtils.h"
@@ -130,6 +131,13 @@ static cl::opt<bool> EnableForwardingConflictDetection(
     "store-to-load-forwarding-conflict-detection", cl::Hidden,
     cl::desc("Enable conflict detection in loop-access analysis"),
     cl::init(true));
+
+/// Enable analysis using Tapir based on the data-race-free assumption.
+static cl::opt<bool> EnableDRFAA(
+    "enable-drf-laa", cl::Hidden,
+    cl::desc("Enable analysis using Tapir based on the data-race-free "
+             "assumption"),
+    cl::init(false));
 
 bool VectorizerParams::isInterleaveForced() {
   return ::VectorizationInterleave.getNumOccurrences() > 0;
@@ -1317,6 +1325,12 @@ bool MemoryDepChecker::couldPreventStoreLoadForward(uint64_t Distance,
   return false;
 }
 
+/// Returns true if this loop is logically parallel as indicated by Tapir.
+static bool isLogicallyParallelViaTapir(const Loop *L, TaskInfo *TI) {
+  return L->wasDerivedFromTapirLoop() ||
+    (TI && getTaskIfTapirLoopStructure(L, TI));
+}
+
 /// Given a non-constant (unknown) dependence-distance \p Dist between two
 /// memory accesses, that have the same stride whose absolute value is given
 /// in \p Stride, and that have the same type size \p TypeByteSize,
@@ -1433,6 +1447,11 @@ MemoryDepChecker::isDependent(const MemAccessInfo &A, unsigned AIdx,
 
   // Two reads are independent.
   if (!AIsWrite && !BIsWrite)
+    return Dependence::NoDep;
+
+  // Under certain assumptions, Tapir can guarantee that there are no
+  // loop-carried dependencies.
+  if (EnableDRFAA && isLogicallyParallelViaTapir(InnermostLoop, TI))
     return Dependence::NoDep;
 
   // We cannot check pointers in different address spaces.
@@ -1652,6 +1671,12 @@ bool MemoryDepChecker::areDepsSafe(DepCandidates &AccessSets,
 
             Dependence::DepType Type =
                 isDependent(*A.first, A.second, *B.first, B.second, Strides);
+            // Backward dependencies cannot happen in Tapir loops.
+            if ((Dependence::Backward == Type ||
+                 Dependence::BackwardVectorizable == Type ||
+                 Dependence::BackwardVectorizableButPreventsForwarding == Type)
+                && isLogicallyParallelViaTapir(InnermostLoop, TI))
+              Type = Dependence::NoDep;
             SafeForVectorization &= Dependence::isSafeForVectorization(Type);
 
             // Gather dependences unless we accumulated MaxDependences
@@ -1762,7 +1787,7 @@ bool LoopAccessInfo::canAnalyzeLoop() {
 
 void LoopAccessInfo::analyzeLoop(AliasAnalysis *AA, LoopInfo *LI,
                                  const TargetLibraryInfo *TLI,
-                                 DominatorTree *DT) {
+                                 DominatorTree *DT, TaskInfo *TI) {
   typedef SmallPtrSet<Value*, 16> ValueSet;
 
   // Holds the Load and Store instructions.
@@ -1776,7 +1801,8 @@ void LoopAccessInfo::analyzeLoop(AliasAnalysis *AA, LoopInfo *LI,
   PtrRtChecking->Pointers.clear();
   PtrRtChecking->Need = false;
 
-  const bool IsAnnotatedParallel = TheLoop->isAnnotatedParallel();
+  const bool IsAnnotatedParallel = TheLoop->isAnnotatedParallel() ||
+    (EnableDRFAA && isLogicallyParallelViaTapir(TheLoop, TI));
 
   // For each block.
   for (BasicBlock *BB : TheLoop->blocks()) {
@@ -2270,14 +2296,14 @@ void LoopAccessInfo::collectStridedAccess(Value *MemAccess) {
 
 LoopAccessInfo::LoopAccessInfo(Loop *L, ScalarEvolution *SE,
                                const TargetLibraryInfo *TLI, AliasAnalysis *AA,
-                               DominatorTree *DT, LoopInfo *LI)
+                               DominatorTree *DT, LoopInfo *LI, TaskInfo *TI)
     : PSE(llvm::make_unique<PredicatedScalarEvolution>(*SE, *L)),
       PtrRtChecking(llvm::make_unique<RuntimePointerChecking>(SE)),
       DepChecker(llvm::make_unique<MemoryDepChecker>(*PSE, L)), TheLoop(L),
       NumLoads(0), NumStores(0), MaxSafeDepDistBytes(-1), CanVecMem(false),
       StoreToLoopInvariantAddress(false) {
   if (canAnalyzeLoop())
-    analyzeLoop(AA, LI, TLI, DT);
+    analyzeLoop(AA, LI, TLI, DT, TI);
 }
 
 void LoopAccessInfo::print(raw_ostream &OS, unsigned Depth) const {
@@ -2347,6 +2373,8 @@ bool LoopAccessLegacyAnalysis::runOnFunction(Function &F) {
   AA = &getAnalysis<AAResultsWrapperPass>().getAAResults();
   DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
   LI = &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+  if (EnableDRFAA)
+    TI = &getAnalysis<TaskInfoWrapperPass>().getTaskInfo();
 
   return false;
 }
@@ -2356,6 +2384,8 @@ void LoopAccessLegacyAnalysis::getAnalysisUsage(AnalysisUsage &AU) const {
     AU.addRequired<AAResultsWrapperPass>();
     AU.addRequired<DominatorTreeWrapperPass>();
     AU.addRequired<LoopInfoWrapperPass>();
+    if (EnableDRFAA)
+      AU.addRequired<TaskInfoWrapperPass>();
 
     AU.setPreservesAll();
 }
@@ -2369,13 +2399,15 @@ INITIALIZE_PASS_DEPENDENCY(AAResultsWrapperPass)
 INITIALIZE_PASS_DEPENDENCY(ScalarEvolutionWrapperPass)
 INITIALIZE_PASS_DEPENDENCY(DominatorTreeWrapperPass)
 INITIALIZE_PASS_DEPENDENCY(LoopInfoWrapperPass)
+INITIALIZE_PASS_DEPENDENCY(TaskInfoWrapperPass)
 INITIALIZE_PASS_END(LoopAccessLegacyAnalysis, LAA_NAME, laa_name, false, true)
 
 AnalysisKey LoopAccessAnalysis::Key;
 
 LoopAccessInfo LoopAccessAnalysis::run(Loop &L, LoopAnalysisManager &AM,
                                        LoopStandardAnalysisResults &AR) {
-  return LoopAccessInfo(&L, &AR.SE, &AR.TLI, &AR.AA, &AR.DT, &AR.LI);
+  return LoopAccessInfo(&L, &AR.SE, &AR.TLI, &AR.AA, &AR.DT, &AR.LI,
+                        EnableDRFAA ? &AR.TI : nullptr);
 }
 
 namespace llvm {

tarunprabhu/kitsune

llvm/include/llvm/Analysis/LoopAccessAnalysis.h
@@ -169,8 +169,9 @@ public:
                const SmallVectorImpl<Instruction *> &Instrs) const;
   };
 
-  MemoryDepChecker(PredicatedScalarEvolution &PSE, const Loop *L)
-      : PSE(PSE), InnermostLoop(L) {}
+  MemoryDepChecker(PredicatedScalarEvolution &PSE, const Loop *L,
+                   TaskInfo *TI = nullptr)
+      : PSE(PSE), InnermostLoop(L), TI(TI) {}
 
   /// Register the location (instructions are given increasing numbers)
   /// of a write access.
@@ -301,6 +302,9 @@ private:
   /// Memory dependences collected during the analysis.  Only valid if
   /// RecordDependences is true.
   SmallVector<Dependence, 8> Dependences;
+
+  /// Optional TaskInfo
+  TaskInfo *TI;
 
   /// Check whether there is a plausible dependence between the two
   /// accesses.
@@ -562,7 +566,8 @@ private:
 class LoopAccessInfo {
 public:
   LoopAccessInfo(Loop *L, ScalarEvolution *SE, const TargetLibraryInfo *TLI,
-                 AAResults *AA, DominatorTree *DT, LoopInfo *LI);
+                 AAResults *AA, DominatorTree *DT, LoopInfo *LI,
+                 TaskInfo *TI = nullptr);
 
   /// Return true we can analyze the memory accesses in the loop and there are
   /// no memory dependence cycles.
@@ -641,7 +646,8 @@ public:
 private:
   /// Analyze the loop.
   void analyzeLoop(AAResults *AA, LoopInfo *LI,
-                   const TargetLibraryInfo *TLI, DominatorTree *DT);
+                   const TargetLibraryInfo *TLI, DominatorTree *DT,
+                   TaskInfo *TI);
 
   /// Check if the structure of the loop allows it to be analyzed by this
   /// pass.
@@ -775,12 +781,14 @@ class LoopAccessInfoManager {
   AAResults &AA;
   DominatorTree &DT;
   LoopInfo &LI;
+  TaskInfo *TI = nullptr;
   const TargetLibraryInfo *TLI = nullptr;
 
 public:
   LoopAccessInfoManager(ScalarEvolution &SE, AAResults &AA, DominatorTree &DT,
-                        LoopInfo &LI, const TargetLibraryInfo *TLI)
-      : SE(SE), AA(AA), DT(DT), LI(LI), TLI(TLI) {}
+                        LoopInfo &LI, const TargetLibraryInfo *TLI,
+                        TaskInfo *TI)
+      : SE(SE), AA(AA), DT(DT), LI(LI), TLI(TLI), TI(TI) {}
 
   const LoopAccessInfo &getInfo(Loop &L);
 

llvm/lib/Analysis/LoopAccessAnalysis.cpp
@@ -32,6 +32,7 @@
 #include "llvm/Analysis/OptimizationRemarkEmitter.h"
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Analysis/ScalarEvolutionExpressions.h"
+#include "llvm/Analysis/TapirTaskInfo.h"
 #include "llvm/Analysis/TargetLibraryInfo.h"
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/Analysis/VectorUtils.h"
@@ -135,6 +136,12 @@ static cl::opt<unsigned> MaxForkedSCEVDepth(
     "max-forked-scev-depth", cl::Hidden,
     cl::desc("Maximum recursion depth when finding forked SCEVs (default = 5)"),
     cl::init(5));
+/// Enable analysis using Tapir based on the data-race-free assumption.
+static cl::opt<bool> EnableDRFAA(
+    "enable-drf-laa", cl::Hidden,
+    cl::desc("Enable analysis using Tapir based on the data-race-free "
+             "assumption"),
+    cl::init(false));
 
 bool VectorizerParams::isInterleaveForced() {
   return ::VectorizationInterleave.getNumOccurrences() > 0;
@@ -1724,6 +1731,12 @@ void MemoryDepChecker::mergeInStatus(VectorizationSafetyStatus S) {
     Status = S;
 }
 
+/// Returns true if this loop is logically parallel as indicated by Tapir.
+static bool isLogicallyParallelViaTapir(const Loop *L, TaskInfo *TI) {
+  return L->wasDerivedFromTapirLoop() ||
+    (TI && getTaskIfTapirLoopStructure(L, TI));
+}
+
 /// Given a dependence-distance \p Dist between two
 /// memory accesses, that have the same stride whose absolute value is given
 /// in \p Stride, and that have the same type size \p TypeByteSize,
@@ -1840,6 +1853,11 @@ MemoryDepChecker::isDependent(const MemAccessInfo &A, unsigned AIdx,
 
   // Two reads are independent.
   if (!AIsWrite && !BIsWrite)
+    return Dependence::NoDep;
+
+  // Under certain assumptions, Tapir can guarantee that there are no
+  // loop-carried dependencies.
+  if (EnableDRFAA && isLogicallyParallelViaTapir(InnermostLoop, TI))
     return Dependence::NoDep;
 
   // We cannot check pointers in different address spaces.
@@ -2069,6 +2087,12 @@ bool MemoryDepChecker::areDepsSafe(DepCandidates &AccessSets,
 
             Dependence::DepType Type =
                 isDependent(*A.first, A.second, *B.first, B.second, Strides);
+            // Backward dependencies cannot happen in Tapir loops.
+            if ((Dependence::Backward == Type ||
+                 Dependence::BackwardVectorizable == Type ||
+                 Dependence::BackwardVectorizableButPreventsForwarding == Type)
+                && isLogicallyParallelViaTapir(InnermostLoop, TI))
+              Type = Dependence::NoDep;
             mergeInStatus(Dependence::isSafeForVectorization(Type));
 
             // Gather dependences unless we accumulated MaxDependences
@@ -2159,7 +2183,9 @@ bool LoopAccessInfo::canAnalyzeLoop() {
 
 void LoopAccessInfo::analyzeLoop(AAResults *AA, LoopInfo *LI,
                                  const TargetLibraryInfo *TLI,
-                                 DominatorTree *DT) {
+                                 DominatorTree *DT, TaskInfo *TI) {
+  typedef SmallPtrSet<Value*, 16> ValueSet;
+
   // Holds the Load and Store instructions.
   SmallVector<LoadInst *, 16> Loads;
   SmallVector<StoreInst *, 16> Stores;
@@ -2176,7 +2202,8 @@ void LoopAccessInfo::analyzeLoop(AAResults *AA, LoopInfo *LI,
   PtrRtChecking->Pointers.clear();
   PtrRtChecking->Need = false;
 
-  const bool IsAnnotatedParallel = TheLoop->isAnnotatedParallel();
+  const bool IsAnnotatedParallel = TheLoop->isAnnotatedParallel() ||
+    (EnableDRFAA && isLogicallyParallelViaTapir(TheLoop, TI));
 
   const bool EnableMemAccessVersioningOfLoop =
       EnableMemAccessVersioning &&
@@ -2409,7 +2436,7 @@ void LoopAccessInfo::analyzeLoop(AAResults *AA, LoopInfo *LI,
                                SymbolicStrides, UncomputablePtr, false);
   if (!CanDoRTIfNeeded) {
     auto *I = dyn_cast_or_null<Instruction>(UncomputablePtr);
-    recordAnalysis("CantIdentifyArrayBounds", I) 
+    recordAnalysis("CantIdentifyArrayBounds", I)
         << "cannot identify array bounds";
     LLVM_DEBUG(dbgs() << "LAA: We can't vectorize because we can't find "
                       << "the array bounds.\n");
@@ -2627,14 +2654,13 @@ void LoopAccessInfo::collectStridedAccess(Value *MemAccess) {
 
 LoopAccessInfo::LoopAccessInfo(Loop *L, ScalarEvolution *SE,
                                const TargetLibraryInfo *TLI, AAResults *AA,
-                               DominatorTree *DT, LoopInfo *LI)
+                               DominatorTree *DT, LoopInfo *LI, TaskInfo *TI)
     : PSE(std::make_unique<PredicatedScalarEvolution>(*SE, *L)),
       PtrRtChecking(nullptr),
       DepChecker(std::make_unique<MemoryDepChecker>(*PSE, L)), TheLoop(L) {
   PtrRtChecking = std::make_unique<RuntimePointerChecking>(*DepChecker, SE);
-  if (canAnalyzeLoop()) {
-    analyzeLoop(AA, LI, TLI, DT);
-  }
+  if (canAnalyzeLoop())
+    analyzeLoop(AA, LI, TLI, DT, TI);
 }
 
 void LoopAccessInfo::print(raw_ostream &OS, unsigned Depth) const {
@@ -2701,7 +2727,10 @@ bool LoopAccessLegacyAnalysis::runOnFunction(Function &F) {
   auto &AA = getAnalysis<AAResultsWrapperPass>().getAAResults();
   auto &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
   auto &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-  LAIs = std::make_unique<LoopAccessInfoManager>(SE, AA, DT, LI, TLI);
+  TaskInfo *TI = nullptr;
+  if (EnableDRFAA)
+    TI = &getAnalysis<TaskInfoWrapperPass>().getTaskInfo();
+  LAIs = std::make_unique<LoopAccessInfoManager>(SE, AA, DT, LI, TLI, TI);
   return false;
 }
 
@@ -2710,6 +2739,8 @@ void LoopAccessLegacyAnalysis::getAnalysisUsage(AnalysisUsage &AU) const {
   AU.addRequiredTransitive<AAResultsWrapperPass>();
   AU.addRequiredTransitive<DominatorTreeWrapperPass>();
   AU.addRequiredTransitive<LoopInfoWrapperPass>();
+  if (EnableDRFAA)
+    AU.addRequiredTransitive<TaskInfoWrapperPass>();
 
   AU.setPreservesAll();
 }
@@ -2731,6 +2762,7 @@ INITIALIZE_PASS_DEPENDENCY(AAResultsWrapperPass)
 INITIALIZE_PASS_DEPENDENCY(ScalarEvolutionWrapperPass)
 INITIALIZE_PASS_DEPENDENCY(DominatorTreeWrapperPass)
 INITIALIZE_PASS_DEPENDENCY(LoopInfoWrapperPass)
+INITIALIZE_PASS_DEPENDENCY(TaskInfoWrapperPass)
 INITIALIZE_PASS_END(LoopAccessLegacyAnalysis, LAA_NAME, laa_name, false, true)
 
 AnalysisKey LoopAccessAnalysis::Key;