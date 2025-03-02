a6b253f53ad3da551a933c482bc3cf1a16d96628
Tarun Prabhu <tarun.prabhu@gmail.com>
2025-03-02 14:18:33 -0700

    [kitsune] Remove obsolete tests

--- kitsune/tests.old/Realm/realm_init_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "kitsune_realm_c.h"
-
-int main(int argc, char *argv[]) {
-  realmInitRuntime(argc, argv);
-  printf("number of realm processors: %ld\n", realmGetNumProcs());
-  return 0;
-}
