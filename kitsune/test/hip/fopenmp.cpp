9e857741d47fdf7bd0a4e7075b3bb6c3e0e47c9b
Tarun Prabhu <tarun@lanl.gov>
2025-04-29 13:32:09 -0600

    [kitsune/test] Fix typos in tests

--- kitsune/test/hip/fopenmp.cpp
+++ kitsune/test/hip/fopenmp.cpp
@@ -2,7 +2,8 @@
 // OpenMP offload cannot be used together with a Kitsune tapir target.
 //
 // RUN: not %kitxx -### -fopenmp -fopenmp-targets=amdgcn-amd-amdhsa \
-// RUN:     -offload-gpu-arch=gfx90a --tapir=hip -nogpulib -c -O2 %s 2>&1 \
+// RUN:     --offload-arch=gfx90a -nogpulib -c -O2 %s \
+// RUN:     --tapir=hip --tapir-hip-arch=gfx90a  2>&1 \
 // RUN:     | FileCheck %s -check-prefix=TAPIR
 //
 // TAPIR: cannot use OpenMP offload with a tapir target
@@ -11,7 +12,7 @@
 // Running the kitsune frontend without --tapir is allowed.
 //
 // RUN: %kitxx -### -fopenmp -fopenmp-targets=amdgcn-amd-amdhsa \
-// RUN:     -offload-gpu-arch=gfx90a -nogpulib -c -O2 %s 2>&1 \
+// RUN:     --offload-arch=gfx90a -nogpulib -c -O2 %s 2>&1 \
 // RUN:     | FileCheck %s -check-prefix=NOTAPIR
 //
 // NOTAPIR-NOT: cannot use OpenMP offload with a tapir target
--- kitsune/test/hip/fopenmp.f90
+++ kitsune/test/hip/fopenmp.f90
@@ -4,7 +4,8 @@
 ! OpenMP offload cannot be used together with a Kitsune tapir target.
 !
 ! RUN: not %kitfc -### -fopenmp -fopenmp-targets=amdgcn-amd-amdhsa \
-! RUN:     -offload-gpu-arch=gfx90a --tapir=hip -nogpulib -c -O2 %s 2>&1 \
+! RUN:     --offload-arch=gfx90a -nogpulib -c -O2 %s \
+! RUN:     --tapir=hip --tapir-hip-arch=gfx90a 2>&1 \
 ! RUN:     | FileCheck %s -check-prefix=TAPIR
 !
 ! TAPIR: cannot use OpenMP offload with a tapir target
@@ -13,7 +14,7 @@
 ! Running the kitsune frontend without --tapir is allowed.
 !
 ! RUN: %kitfc -### -fopenmp -fopenmp-targets=amdgcn-amd-amdhsa \
-! RUN:     -offload-gpu-arch=gfx90a -nogpulib -c -O2 %s 2>&1 \
+! RUN:     --offload-arch=gfx90a -nogpulib -c -O2 %s 2>&1 \
 ! RUN:     | FileCheck %s -check-prefix=NOTAPIR
 !
 ! NOTAPIR-NOT: cannot use OpenMP offload with a tapir target
