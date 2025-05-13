90d183d374614568a18faff69722e423ed973262
Tarun Prabhu <tarun@lanl.gov>
2025-05-13 11:22:16 -0600

    [kitsune] Fix kitsune tests
    The C++ frontends and clang were being used in the .f90 tests. Fix those to
    use kitfc and flang respectively (sloppy copy-paste bugs). Also fix the test
    that checks that we do not allow OpenMP offload and Tapir simultaneously. The
    test now only emits the error that we are interested no other. Earlier, both
    errors were being emitted, so the test would pass. This is cleaner.

--- kitsune/test/cuda/fopenmp.f90
+++ kitsune/test/cuda/fopenmp.f90
@@ -3,20 +3,18 @@
 ! ------------------------------------------------------------------------------
 ! OpenMP offload cannot be used together with a Kitsune tapir target.
 !
-! RUN: not %kitxx -### -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda \
-! RUN:     --cuda-gpu-arch=sm_80 -nocudalib -c -O2 %s \
-! RUN:     --tapir=cuda --tapir-cuda-arch=sm_80 \
-! RUN:     --libomptarget-nvptx-bc-path=%S/input/nvptx.bc 2>&1 \
+! RUN: not %kitfc -### -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda \
+! RUN:     --offload-arch=sm_80 -nogpulib -c -O2 %s \
+! RUN:     --tapir=cuda --tapir-cuda-arch=sm_80 2>&1 \
 ! RUN:     | FileCheck %s -check-prefix=TAPIR
 !
 ! TAPIR: cannot use OpenMP offload with a tapir target
 !
 ! ------------------------------------------------------------------------------
-! Running the kitsune frontend without --tapir is ok
+! Running the kitsune frontend without --tapir is ok.
 !
-! RUN: %kitxx -### -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda \
-! RUN:     --cuda-gpu-arch=sm_80 -nocudalib -c -O2 %s \
-! RUN:     --libomptarget-nvptx-bc-path=%S/input/nvptx.bc 2>&1 \
+! RUN: %kitfc -### -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda \
+! RUN:     --offload-arch=sm_80 -nogpulib -c -O2 %s 2>&1 \
 ! RUN:     | FileCheck %s -check-prefix=NOTAPIR
 !
 ! NOTAPIR-NOT: cannot use OpenMP offload with a tapir target
