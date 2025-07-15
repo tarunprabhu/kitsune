dcd98df7253ac236e156454be1bea017db2a8ca4
Tarun Prabhu <tarun@lanl.gov>
2025-07-14 19:07:51 -0600

    [kitsune] Share some code between CudaABI and HipABI
    Much of the code that cloned global values used in the tapir loop into the
    device module from the host was duplicated in the cuda and hip tapir targets.
    This has been refactored so it is shared by the two.

--- llvm/lib/Transforms/Tapir/KitsuneUtils.cpp
+++ llvm/lib/Transforms/Tapir/KitsuneUtils.cpp
@@ -101,6 +101,143 @@ void llvm::collectGlobalValues(Loop &loop, std::set<GlobalValue *> &seen) {
     collectGlobalValues(*bb, seen);
 }
 
+void llvm::cloneUsedGlobalVariablesInto(
+    Module &devM, const std::set<GlobalValue *> &usedGlobalValues,
+    ValueToValueMapTy &vmap, unsigned asConst, unsigned asNonConst,
+    GlobalValue::VisibilityTypes visConst,
+    GlobalValue::VisibilityTypes visNonConst) {
+  for (GlobalValue *v : usedGlobalValues) {
+    auto *g = dyn_cast<GlobalVariable>(v);
+    if (not g)
+      continue;
+
+    assert(g->getType()->getAddressSpace() == 0 &&
+           "Global variables must be in default address space");
+
+    // It would be nice to do this as a "post-processing" pass, for instance,
+    // when preparing the kernel module for PTX. However, we need to pass the
+    // name of the global as a string to several Kitsune intrinsics. That would
+    // make the job of renaming them later much more complicated since we would
+    // have to modify all the calls in the host that use the global variable's
+    // name.
+    StringRef name = g->getName();
+    bool isConst = g->isConstant();
+    Type *type = g->getValueType();
+    MaybeAlign align = g->getAlign();
+    GlobalValue::ThreadLocalMode threadLocalMode = g->getThreadLocalMode();
+
+    GlobalVariable *newg = nullptr;
+    if ((newg = devM.getGlobalVariable(name, /*AllowLocal=*/true))) {
+      // If a global with the name is already present in the kernel module,
+      // another outlined loop in the host module used the same global. The
+      // global is already present, so we just need to update VMap correctly.
+      // This is done after this if-else block.
+    } else if (isConst) {
+      // If the global variable is a constant we can clone it into the device
+      // module along with its initializer where it will be treated as an
+      // internal variable. There is no coordination with the host.
+      Constant *init = g->getInitializer();
+      newg = new GlobalVariable(
+          devM, type, isConst, GlobalValue::InternalLinkage, init, name,
+          /*InsertBefore=*/nullptr, threadLocalMode, asConst);
+      newg->setDSOLocal(true);
+      newg->setAlignment(align);
+      newg->setVisibility(visConst);
+    } else {
+      // If the global is not constant, we will need to create a device-side
+      // version that will have the host-side value copied over prior to
+      // launching the kernel.
+      Constant *init = Constant::getNullValue(type);
+      newg = new GlobalVariable(
+          devM, type, isConst, GlobalValue::ExternalLinkage, init, name,
+          /*InsertBefore=*/nullptr, threadLocalMode, asNonConst);
+      newg->setDSOLocal(true);
+      newg->setAlignment(align);
+      newg->setVisibility(visNonConst);
+    }
+
+    // This is really just a sanity check in case the code above changes and
+    // someone makes a silly mistake.
+    assert(newg && "All global variables must have a corresponding global "
+                   "in the kernel module");
+
+    // The global variables are assumed to be in the default address space when
+    // outlining. All uses of the global expect them to be in the default
+    // address space. If they are not, cast them in the vmap so when we clone
+    // any entities that use them, we do not have type mismatches.
+    if (newg->getType()->getAddressSpace()) {
+      LLVMContext &ctx = devM.getContext();
+      PointerType *ptrTy = PointerType::getUnqual(ctx);
+      vmap[g] = ConstantExpr::getAddrSpaceCast(newg, ptrTy);
+    } else {
+      vmap[g] = newg;
+    }
+  }
+}
+
+void llvm::cloneReachableFuncsInto(
+    Module &devM, const std::set<GlobalValue *> &usedGlobalValues,
+    ValueToValueMapTy &vmap) {
+  // Functions that are called from the tapir loop must be cloned into the
+  // kernel module, especially if they contain a body. This is a two-step
+  // process - first we create a declaration for the functions since these may
+  // be called by the other reachable functions. The VMap already contains
+  // mappings for the global variables that may be needed.
+  for (GlobalValue *g : usedGlobalValues) {
+    if (auto *f = dyn_cast<Function>(g)) {
+      StringRef fname = f->getName();
+      Function *devf = devM.getFunction(fname);
+      if (not devf) {
+        FunctionType *fty = f->getFunctionType();
+        GlobalValue::LinkageTypes linkage = f->getLinkage();
+        devf = Function::Create(fty, linkage, fname, devM);
+        for (unsigned i = 0; i < f->arg_size(); ++i) {
+          Argument *a = f->getArg(i);
+          Argument *deva = devf->getArg(i);
+          deva->setName(a->getName());
+          vmap[a] = deva;
+        }
+      }
+      vmap[f] = devf;
+    }
+  }
+
+  // The vmap now contains mappings from all functions in the source module to
+  // their counterparts in the device module. It is now safe to clone the bodies
+  // of the functions.
+  for (GlobalValue *g : usedGlobalValues) {
+    if (auto *f = dyn_cast<Function>(g)) {
+      if (f->size() and not f->isIntrinsic()) {
+        SmallVector<ReturnInst *, 8> returns;
+        auto *devf = cast<Function>(vmap[f]);
+        CloneFunctionInto(devf, f, vmap,
+                          CloneFunctionChangeType::DifferentModule, returns);
+        devf->addFnAttr(Attribute::KitDevice);
+      }
+    }
+  }
+}
+
+void llvm::cloneReachableIFuncsInto(
+    Module &devM, const std::set<GlobalValue *> &usedGlobalValues,
+    ValueToValueMapTy &vmap) {
+  // IFunc's are a GNU extension, and it is unlikely that we will ever compile
+  // code that uses them.
+  for (GlobalValue *v : usedGlobalValues)
+    if (isa<GlobalIFunc>(v))
+      llvm_unreachable("cloneReachableIFuncsInto: not yet implemented");
+}
+
+void llvm::cloneUsedGlobalAliasesInto(
+    Module &devM, const std::set<GlobalValue *> &usedGlobalValues,
+    ValueToValueMapTy &vmap) {
+  // FIXME: At some point, we should support global aliases, but right now,
+  // there are a number of other features that need to be supported.
+  for (GlobalValue *v : usedGlobalValues)
+    if (isa<GlobalAlias>(v))
+      llvm_unreachable("cloneUsedGlobalAliasesInto: not yet implemented");
+}
+
 std::string llvm::getNameForTapirLoop(const TapirLoopInfo &tl, StringRef pfx,
                                       unsigned suffix) {
   std::string buf;
