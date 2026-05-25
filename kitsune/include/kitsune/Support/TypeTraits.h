6acbdb9a7918fd46d57f970500b4861af03396c0
Tarun Prabhu <tarun@lanl.gov>
2026-05-25 11:26:55 -0600

    [kitsune] Add convenient type traits
    These make it easier to query for specific types than the traits available in
    the standard C++ headers.

---
+++ kitsune/include/kitsune/Support/TypeTraits.h
@@ -0,0 +1,76 @@
+//===- TypeTraits.h - Type traits ------------------------------*- C++ -*--===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Convenience type traits that are not part of the standard headers.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KITSUNE_SUPPORT_TYPE_TRAITS_H
+#define KITSUNE_SUPPORT_TYPE_TRAITS_H
+
+#include <type_traits>
+
+// These are added to the std namespace for consistency with other standard
+// traits.
+namespace std {
+
+/// \addtogroup kitsune
+/// @{
+
+/// Return true if the type is a boolean. This will not return true if `T` is
+/// bool&.
+template <typename T>
+static constexpr bool is_bool_v = std::is_same_v<std::remove_cv_t<T>, bool>;
+
+/// Check if the type is an integer, but not a boolean. This will return true
+/// for the following types and their aliases:
+///
+///     int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t
+///
+/// as well as the cv-qualified variants of these. This will not return true if
+/// `T` is a reference type.
+template <typename T>
+static constexpr bool is_integer_v =
+    std::is_integral_v<std::remove_cv_t<T>> && !std::is_bool_v<T>;
+
+/// Check if the type is a 32-bit floating point type. This will return on the
+/// following types and their aliases:
+///
+///     float
+///
+/// as we as the cv-qualified variants of these. This will not return true if
+/// `T` is a reference type.
+template <typename T>
+static constexpr bool is_float_v = std::is_same_v<std::remove_cv_t<T>, float>;
+
+/// Check if the type is a 64-bit floating point type. This will return on the
+/// following types and their aliases:
+///
+///     double
+///
+/// as we as the cv-qualified variants of these. This will not return true if
+/// `T` is a reference type.
+template <typename T>
+static constexpr bool is_double_v = std::is_same_v<std::remove_cv_t<T>, double>;
+
+/// Check if the type is a long double type. This will return on the following
+/// types and their aliases:
+///
+///     long double
+///
+/// as we as the cv-qualified variants of these. This will not return true if
+/// `T` is a reference type.
+template <typename T>
+static constexpr bool is_long_double_v =
+    std::is_same_v<std::remove_cv_t<T>, long double>;
+
+/// @}
+
+} // namespace std
+
+#endif // KITSUNE_SUPPORT_TYPE_TRAITS_H
---
+++ kitsune/unittests/Support/TypeTraitsTest.cpp
@@ -0,0 +1,91 @@
+//===- TypeTraitsTest.cpp - Unit tests for type traits --------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "kitsune/Support/TypeTraits.h"
+
+#include "gtest/gtest.h"
+
+namespace {
+
+using Bool = bool;
+
+TEST(KitTypeTraits, isBool) {
+  EXPECT_TRUE(std::is_bool_v<bool>);
+  EXPECT_TRUE(std::is_bool_v<const bool>);
+  EXPECT_TRUE(std::is_bool_v<Bool>);
+
+  EXPECT_FALSE(std::is_bool_v<bool &>);
+  EXPECT_FALSE(std::is_bool_v<const bool &>);
+
+  EXPECT_FALSE(std::is_bool_v<int8_t>);
+  EXPECT_FALSE(std::is_bool_v<uint8_t>);
+  EXPECT_FALSE(std::is_bool_v<unsigned char>);
+}
+
+TEST(KitTypeTraits, isInteger) {
+  EXPECT_TRUE(std::is_integer_v<int8_t>);
+  EXPECT_TRUE(std::is_integer_v<int16_t>);
+  EXPECT_TRUE(std::is_integer_v<int32_t>);
+  EXPECT_TRUE(std::is_integer_v<int64_t>);
+  EXPECT_TRUE(std::is_integer_v<uint8_t>);
+  EXPECT_TRUE(std::is_integer_v<uint16_t>);
+  EXPECT_TRUE(std::is_integer_v<uint32_t>);
+  EXPECT_TRUE(std::is_integer_v<uint64_t>);
+
+  EXPECT_TRUE(std::is_integer_v<char>);
+  EXPECT_TRUE(std::is_integer_v<signed char>);
+  EXPECT_TRUE(std::is_integer_v<unsigned char>);
+  EXPECT_TRUE(std::is_integer_v<short>);
+  EXPECT_TRUE(std::is_integer_v<int>);
+  EXPECT_TRUE(std::is_integer_v<long>);
+  EXPECT_TRUE(std::is_integer_v<long long>);
+  EXPECT_TRUE(std::is_integer_v<unsigned>);
+  EXPECT_TRUE(std::is_integer_v<unsigned char>);
+  EXPECT_TRUE(std::is_integer_v<unsigned short>);
+  EXPECT_TRUE(std::is_integer_v<unsigned int>);
+  EXPECT_TRUE(std::is_integer_v<unsigned long>);
+  EXPECT_TRUE(std::is_integer_v<unsigned long long>);
+
+  EXPECT_TRUE(std::is_integer_v<const int8_t>);
+  EXPECT_TRUE(std::is_integer_v<const int16_t>);
+  EXPECT_TRUE(std::is_integer_v<const int32_t>);
+  EXPECT_TRUE(std::is_integer_v<const int64_t>);
+  EXPECT_TRUE(std::is_integer_v<const uint8_t>);
+  EXPECT_TRUE(std::is_integer_v<const uint16_t>);
+  EXPECT_TRUE(std::is_integer_v<const uint32_t>);
+  EXPECT_TRUE(std::is_integer_v<const uint64_t>);
+
+  EXPECT_TRUE(std::is_integer_v<const char>);
+  EXPECT_TRUE(std::is_integer_v<const signed char>);
+  EXPECT_TRUE(std::is_integer_v<const unsigned char>);
+  EXPECT_TRUE(std::is_integer_v<const short>);
+  EXPECT_TRUE(std::is_integer_v<const int>);
+  EXPECT_TRUE(std::is_integer_v<const long>);
+  EXPECT_TRUE(std::is_integer_v<const long long>);
+  EXPECT_TRUE(std::is_integer_v<const unsigned>);
+  EXPECT_TRUE(std::is_integer_v<const unsigned char>);
+  EXPECT_TRUE(std::is_integer_v<const unsigned short>);
+  EXPECT_TRUE(std::is_integer_v<const unsigned int>);
+  EXPECT_TRUE(std::is_integer_v<const unsigned long>);
+  EXPECT_TRUE(std::is_integer_v<const unsigned long long>);
+
+  EXPECT_FALSE(std::is_integer_v<int8_t &>);
+  EXPECT_FALSE(std::is_integer_v<int16_t &>);
+  EXPECT_FALSE(std::is_integer_v<int32_t &>);
+  EXPECT_FALSE(std::is_integer_v<int64_t &>);
+  EXPECT_FALSE(std::is_integer_v<uint8_t &>);
+  EXPECT_FALSE(std::is_integer_v<uint16_t &>);
+  EXPECT_FALSE(std::is_integer_v<uint32_t &>);
+  EXPECT_FALSE(std::is_integer_v<uint64_t &>);
+
+  EXPECT_FALSE(std::is_integer_v<bool>);
+  EXPECT_FALSE(std::is_integer_v<const bool>);
+  EXPECT_FALSE(std::is_integer_v<Bool>);
+}
+
+} // namespace
