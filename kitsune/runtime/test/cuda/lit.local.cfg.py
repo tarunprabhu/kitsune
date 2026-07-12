c3f6af6190d78f397fcf7af5266f776bac6df2d3
Tarun Prabhu <tarun@lanl.gov>
2026-07-12 16:42:33 -0600

    [kitsune][runtime] Move checks for GPU existence into cuda/hip subdirectories
    Kitsune's runtime tests for the cuda and hip tapir targets require a GPU to be
    present when the tests are run. On some systems, running `nvptx-arch`, or
    `amdgpu-arch` to check for the presence of a GPU can be expensive (taking over a
    second, if not more). To avoid this, the checks for a GPU are moved into the
    directories that contain the tests that are guarded with the `nvidia-gpu` and
    `amd-gpu` features. In addition, the check is only carried out if the
    corresponding tapir target has been built.

--- kitsune/runtime/test/cuda/lit.local.cfg.py
+++ kitsune/runtime/test/cuda/lit.local.cfg.py
@@ -1,2 +1,14 @@
-if 'kitsune-cuda' not in config.available_features:
+import subprocess
+
+# Running `nvptx-arch` can be expensive on certain systems, especially if an
+# NVIDIA GPU is not present on it. Therefore, we perform this check only when we
+# descend into this directory, and only if we know that the `cuda` tapir target
+# has been built.
+def has_gpu():
+    cmd = os.path.join(config.llvm_tools_dir, "nvptx-arch")
+    return len(subprocess.check_output([cmd])) != 0
+
+if "kitsune-cuda" not in config.available_features:
     config.unsupported = True
+elif has_gpu():
+    config.available_features.add("nvidia-gpu")
--- kitsune/runtime/test/hip/lit.local.cfg.py
+++ kitsune/runtime/test/hip/lit.local.cfg.py
@@ -1,2 +1,14 @@
-if 'kitsune-hip' not in config.available_features:
+import subprocess
+
+# Running `amdgpu-arch` can be expensive on certain systems, especially if an
+# AMD GPU is not present on it. Therefore, we perform this check only when we
+# descend into this directory, and only if we know that the `hip` tapir target
+# has been built.
+def has_gpu():
+    cmd = os.path.join(config.llvm_tools_dir, "amdgpu-arch")
+    return len(subprocess.check_output([cmd])) != 0
+
+if "kitsune-hip" not in config.available_features:
     config.unsupported = True
+elif has_gpu():
+    config.available_features.add("amd-gpu")
