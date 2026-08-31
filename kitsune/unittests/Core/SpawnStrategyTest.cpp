b739340c17c175bee1c616b2ffb90a7b48710703
Tarun Prabhu <tarun@lanl.gov>
2026-08-30 20:21:03 -0600

    [kitsune][NFC] Fix file banner
    Fixes the formatting and the description of the tests in the file.

--- kitsune/unittests/Core/SpawnStrategyTest.cpp
+++ kitsune/unittests/Core/SpawnStrategyTest.cpp
@@ -1,5 +1,4 @@
-//===- SpawnStrategyTest.cpp - Tests for the core TTID enum
-//--------------------------===//
+//===- SpawnStrategyTest.cpp - Tests for the SpawnStrategy enum -----------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
